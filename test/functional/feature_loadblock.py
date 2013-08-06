27f68025e9b358b30e173c44c9a9004c140320e8
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:12:54 -0700

    [Net] Change seed servers, and magic strings so as to avoid contact with the Bitcoin network.

--- test/functional/feature_loadblock.py
+++ test/functional/feature_loadblock.py
@@ -63,7 +63,7 @@ class LoadblockTest(FreicoinTestFramework):
             cfg.write(f"host={node_url.hostname}\n")
             cfg.write(f"output_file={bootstrap_file}\n")
             cfg.write(f"max_height=100\n")
-            cfg.write(f"netmagic=fabfb5da\n")
+            cfg.write(f"netmagic=ed999cf6\n")
             cfg.write(f"input={blocks_dir}\n")
             cfg.write(f"genesis={genesis_block}\n")
             cfg.write(f"hashlist={hash_list.name}\n")
