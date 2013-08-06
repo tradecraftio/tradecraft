8a069b35ad82c80757c91f3d6103c55d12425e37
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:12:54 -0700

    [Net] Change network port, and all references to it.

--- qa/rpc-tests/rawtransactions.py
+++ qa/rpc-tests/rawtransactions.py
@@ -37,7 +37,7 @@ class RawTransactionsTest(FreicoinTestFramework):
         self.nodes = start_nodes(self.num_nodes, self.options.tmpdir)
 
         #connect to a local machine for debugging
-        #url = "http://freicoinrpc:DP6DvqZtqXarpeNWyN3LZTFchCCyCUuHwNF7E8pX99x1@%s:%d" % ('127.0.0.1', 18332)
+        #url = "http://freicoinrpc:DP6DvqZtqXarpeNWyN3LZTFchCCyCUuHwNF7E8pX99x1@%s:%d" % ('127.0.0.1', 18638)
         #proxy = AuthServiceProxy(url)
         #proxy.url = url # store URL on proxy for info
         #self.nodes.append(proxy)
