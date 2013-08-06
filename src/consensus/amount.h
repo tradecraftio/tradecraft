04cd8697def5042d41bedd654b4d60efa1d86beb
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:51:38 -0700

    [Subsidy] Change MAX_MONEY to 2^53-1.

--- src/consensus/amount.h
+++ src/consensus/amount.h
@@ -28,13 +28,18 @@ static constexpr CAmount COIN = 100000000;
 /** No amount larger than this (in kria) is valid.
  *
  * Note that this constant is *not* the total money supply, which in Freicoin
- * currently happens to be less than 21,000,000 FRC for various reasons, but
- * rather a sanity check. As this sanity check is used by consensus-critical
+ * currently happens to be more than this value for various reasons, but rather
+ * a sanity check.  As this sanity check is used by consensus-critical
  * validation code, the exact value of the MAX_MONEY constant is consensus
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
+ *
+ * Note also that UNLIKE bitcoin, this is less than the total monetary supply in
+ * Freicoin.  It *is* possible to create a transaction which includes inputs
+ * which exceed this value when combined together.  Such a transaction would be
+ * invalid.
  * */
-static constexpr CAmount MAX_MONEY = 21000000 * COIN;
+static constexpr CAmount MAX_MONEY = 9007199254740991LL; // 2^53 - 1
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /** Adjust to present value by subtracting (or adding) demurrage */
