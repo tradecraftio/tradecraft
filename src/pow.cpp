37da029ecb59c40562b365d3de056145bb84edbe
Mark Friedenbach <mark@friedenbach.org>
2013-08-05 18:12:06 -0700

    [DiffAdjust] Add new difficulty adjustment filter.
    We replace the simple average over 2016 blocks with a faster acting 144-tap Parks and McClellan FIR filter, that adjusts every 9 blocks with a gain of 0.1025 and limiter of 1.055.

--- src/pow.cpp
+++ src/pow.cpp
@@ -21,13 +21,116 @@
 #include <primitives/block.h>
 #include <uint256.h>
 
+int64_t GetActualTimespan(const CBlockIndex* pindexLast, const Consensus::Params& params)
+{
+    // This fixes an issue where a 51% attack can change difficulty at will.
+    // Go back the full period unless it's the first retarget after genesis.
+    // Code courtesy of Art Forz
+    int blocks_to_go_back = params.original_adjust_interval - 1;
+    if ((pindexLast->nHeight+1) != params.original_adjust_interval)
+        blocks_to_go_back = params.original_adjust_interval;
+
+    // Go back by what we want to be 14 days worth of blocks
+    int nHeightFirst = pindexLast->nHeight - blocks_to_go_back;
+    assert(nHeightFirst >= 0);
+    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
+    assert(pindexFirst);
+
+    return pindexLast->GetBlockTime() - pindexFirst->GetBlockTime();
+}
+
+std::pair<int64_t, int64_t> GetOriginalAdjustmentFactor(const CBlockIndex* pindexLast, const Consensus::Params& params)
+{
+    // Limit adjustment step
+    int64_t actual_timespan = GetActualTimespan(pindexLast, params);
+    if (actual_timespan < params.OriginalTargetTimespan()/4)
+        actual_timespan = params.OriginalTargetTimespan()/4;
+    if (actual_timespan > params.OriginalTargetTimespan()*4)
+        actual_timespan = params.OriginalTargetTimespan()*4;
+
+    return std::make_pair(params.OriginalTargetTimespan(), actual_timespan);
+}
+
+int64_t GetFilteredTime(const CBlockIndex* pindexLast, const Consensus::Params& params)
+{
+    const std::size_t WINDOW = 144;
+    static int32_t filter_coeff[WINDOW] = {
+         -845859,  -459003,  -573589,  -703227,  -848199, -1008841,
+        -1183669, -1372046, -1573247, -1787578, -2011503, -2243311,
+        -2482346, -2723079, -2964681, -3202200, -3432186, -3650186,
+        -3851924, -4032122, -4185340, -4306430, -4389146, -4427786,
+        -4416716, -4349289, -4220031, -4022692, -3751740, -3401468,
+        -2966915, -2443070, -1825548, -1110759,  -295281,   623307,
+         1646668,  2775970,  4011152,  5351560,  6795424,  8340274,
+         9982332, 11717130, 13539111, 15441640, 17417389, 19457954,
+        21554056, 23695744, 25872220, 28072119, 30283431, 32493814,
+        34690317, 36859911, 38989360, 41065293, 43074548, 45004087,
+        46841170, 48573558, 50189545, 51678076, 53028839, 54232505,
+        55280554, 56165609, 56881415, 57422788, 57785876, 57968085,
+        57968084, 57785876, 57422788, 56881415, 56165609, 55280554,
+        54232505, 53028839, 51678076, 50189545, 48573558, 46841170,
+        45004087, 43074548, 41065293, 38989360, 36859911, 34690317,
+        32493814, 30283431, 28072119, 25872220, 23695744, 21554057,
+        19457953, 17417389, 15441640, 13539111, 11717130,  9982332,
+         8340274,  6795424,  5351560,  4011152,  2775970,  1646668,
+          623307,  -295281, -1110759, -1825548, -2443070, -2966915,
+        -3401468, -3751740, -4022692, -4220031, -4349289, -4416715,
+        -4427787, -4389146, -4306430, -4185340, -4032122, -3851924,
+        -3650186, -3432186, -3202200, -2964681, -2723079, -2482346,
+        -2243311, -2011503, -1787578, -1573247, -1372046, -1183669,
+        -1008841,  -848199,  -703227,  -573589,  -459003,  -845858
+    };
+
+    int32_t time_delta[WINDOW];
+
+    size_t idx = 0;
+    const CBlockIndex *pitr = pindexLast;
+    for (; idx!=WINDOW && pitr && pitr->pprev; ++idx, pitr=pitr->pprev)
+        time_delta[idx] = static_cast<int32_t>(pitr->GetBlockTime() - pitr->pprev->GetBlockTime());
+    for (; idx!=WINDOW; ++idx)
+        time_delta[idx] = static_cast<int32_t>(params.nPowTargetSpacing);
+
+    int64_t filtered_time = 0;
+    for (idx=0; idx<WINDOW; ++idx)
+        filtered_time += (int64_t)filter_coeff[idx] * (int64_t)time_delta[idx];
+
+    return filtered_time;
+}
+
+std::pair<int64_t, int64_t> GetFilteredAdjustmentFactor(const CBlockIndex* pindexLast, const Consensus::Params& params)
+{
+    const std::pair<int16_t, int16_t> gain(41, 400); //!< 0.1025
+    const std::pair<int16_t, int16_t> limiter(211, 200); //!< 1.055
+
+    int64_t filtered_time = GetFilteredTime(pindexLast, params);
+
+    int64_t numerator;
+    int64_t denominator;
+    if (filtered_time < 597105209444LL) {
+        numerator   = limiter.first;
+        denominator = limiter.second;
+    } else if (filtered_time > 1943831401459LL) {
+        numerator   = limiter.second;
+        denominator = limiter.first;
+    } else {
+        numerator = ((int64_t)(gain.first + gain.second) * params.nPowTargetSpacing) << 31;
+        numerator -= (int64_t)gain.first * filtered_time;
+        denominator = ((int64_t)gain.second * params.nPowTargetSpacing) << 31;
+    }
+
+    return std::make_pair(numerator, denominator);
+}
+
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
     assert(pindexLast != nullptr);
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
+    const bool use_filter = (pindexLast->nHeight >= (params.diff_adjust_threshold - 1));
+    const int64_t interval = use_filter ? params.filtered_adjust_interval : params.original_adjust_interval;
+
     // Only change once per difficulty adjustment interval
-    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
+    if ((pindexLast->nHeight+1) % interval != 0)
     {
         if (params.fPowAllowMinDifficultyBlocks)
         {
@@ -40,7 +143,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
             {
                 // Return the last non-special-min-difficulty-rules-block
                 const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
+                while (pindex->pprev && pindex->nHeight % interval != 0 && pindex->nBits == nProofOfWorkLimit)
                     pindex = pindex->pprev;
                 return pindex->nBits;
             }
@@ -48,40 +151,35 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         return pindexLast->nBits;
     }
 
-    // This fixes an issue where a 51% attack can change difficulty at will.
-    // Go back the full period unless it's the first retarget after genesis.
-    // Code courtesy of Art Forz
-    int blocks_to_go_back = params.DifficultyAdjustmentInterval()-1;
-    if ((pindexLast->nHeight+1) != params.DifficultyAdjustmentInterval())
-        blocks_to_go_back = params.DifficultyAdjustmentInterval();
-
-    // Go back by what we want to be 14 days worth of blocks
-    int nHeightFirst = pindexLast->nHeight - blocks_to_go_back;
-    assert(nHeightFirst >= 0);
-    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
-    assert(pindexFirst);
-
-    return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
+    return CalculateNextWorkRequired(pindexLast, params);
 }
 
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
+unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, const Consensus::Params& params)
 {
     if (params.fPowNoRetargeting)
         return pindexLast->nBits;
 
-    // Limit adjustment step
-    int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
-    if (nActualTimespan < params.nPowTargetTimespan/4)
-        nActualTimespan = params.nPowTargetTimespan/4;
-    if (nActualTimespan > params.nPowTargetTimespan*4)
-        nActualTimespan = params.nPowTargetTimespan*4;
+    const bool use_filter = (pindexLast->nHeight >= (params.diff_adjust_threshold - 1));
+
+    std::pair<int64_t, int64_t> adjustment_factor;
+
+    if (use_filter) {
+        adjustment_factor = GetFilteredAdjustmentFactor(pindexLast, params);
+    } else {
+        adjustment_factor = GetOriginalAdjustmentFactor(pindexLast, params);
+    }
+
+    assert(adjustment_factor.first >= 0);
+    assert(adjustment_factor.second > 0);
 
     // Retarget
     const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
     arith_uint256 bnNew;
     bnNew.SetCompact(pindexLast->nBits);
-    bnNew *= nActualTimespan;
-    bnNew /= params.nPowTargetTimespan;
+    arith_uint320 bnTmp(bnNew);
+    bnTmp *= arith_uint320(static_cast<uint64_t>(adjustment_factor.second));
+    bnTmp /= arith_uint320(static_cast<uint64_t>(adjustment_factor.first));
+    assert(bnTmp.TruncateTo256(bnNew));
 
     if (bnNew > bnPowLimit)
         bnNew = bnPowLimit;
@@ -95,9 +193,11 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
 {
     if (params.fPowAllowMinDifficultyBlocks) return true;
 
-    if (height % params.DifficultyAdjustmentInterval() == 0) {
-        int64_t smallest_timespan = params.nPowTargetTimespan/4;
-        int64_t largest_timespan = params.nPowTargetTimespan*4;
+    if (height >= (params.diff_adjust_threshold - 1)) {
+        return true;
+    } else if (height % params.original_adjust_interval == 0) {
+        int64_t smallest_timespan = params.OriginalTargetTimespan()/4;
+        int64_t largest_timespan = params.OriginalTargetTimespan()*4;
 
         const arith_uint256 pow_limit = UintToArith256(params.powLimit);
         arith_uint256 observed_new_target;
@@ -107,7 +207,7 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
         arith_uint256 largest_difficulty_target;
         largest_difficulty_target.SetCompact(old_nbits);
         largest_difficulty_target *= largest_timespan;
-        largest_difficulty_target /= params.nPowTargetTimespan;
+        largest_difficulty_target /= params.OriginalTargetTimespan();
 
         if (largest_difficulty_target > pow_limit) {
             largest_difficulty_target = pow_limit;
@@ -123,7 +223,7 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
         arith_uint256 smallest_difficulty_target;
         smallest_difficulty_target.SetCompact(old_nbits);
         smallest_difficulty_target *= smallest_timespan;
-        smallest_difficulty_target /= params.nPowTargetTimespan;
+        smallest_difficulty_target /= params.OriginalTargetTimespan();
 
         if (smallest_difficulty_target > pow_limit) {
             smallest_difficulty_target = pow_limit;
