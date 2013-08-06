71346e93d3272af047a6b893e7cc1b0f5b75836e
Mark Friedenbach <mark@friedenbach.org>
2013-08-05 18:12:06 -0700

    [DiffAdjust] Add new difficulty adjustment filter.
    We replace the simple average over 2016 blocks with a faster acting 144-tap Parks and McClellan FIR filter, that adjusts every 9 blocks with a gain of 0.1025 and limiter of 1.055.

--- src/pow.h
+++ src/pow.h
@@ -25,8 +25,10 @@ class CBlockHeader;
 class CBlockIndex;
 class uint256;
 
+int64_t GetFilteredTime(const CBlockIndex* pindexLast, const Consensus::Params&);
+
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
+unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, const Consensus::Params&);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, unsigned char bias, const Consensus::Params&);
