4fb83546f32ea2bedb63bb2b7b199a26a7c29087
Mark Friedenbach <mark@friedenbach.org>
2013-08-05 18:12:06 -0700

    [DiffAdjust] Add new difficulty adjustment filter.
    We replace the simple average over 2016 blocks with a faster acting 144-tap Parks and McClellan FIR filter, that adjusts every 9 blocks with a gain of 0.1025 and limiter of 1.055.

--- src/test/pow_tests.cpp
+++ src/test/pow_tests.cpp
@@ -29,6 +29,14 @@ using namespace std;
 
 BOOST_FIXTURE_TEST_SUITE(pow_tests, BasicTestingSetup)
 
+/**
+ * The bitcoin version of CalculateNextWorkRequired is stateless,
+ * which the following tests are written to take advantage of. However
+ * filtered time intervals necessarily cannot be stateless, so in
+ * Freicoin we require that the first parameter be an actual connected
+ * CBlockIndex. That unfortunately invalidates these tests.
+ */
+#if 0
 /* Test calculation of next difficulty target with no constraints applying */
 BOOST_AUTO_TEST_CASE(get_next_work)
 {
@@ -84,6 +92,7 @@ BOOST_AUTO_TEST_CASE(get_next_work_upper_limit_actual)
     pindexLast.nBits = 0x1c387f6f;
     BOOST_CHECK_EQUAL(CalculateNextWorkRequired(&pindexLast, nLastRetargetTime, params), 0x1d00e1fd);
 }
+#endif
 
 BOOST_AUTO_TEST_CASE(GetBlockProofEquivalentTime_test)
 {
