a9a1117ef565a20a373b82d1305a3dfe25be1147
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:12:54 -0700

    [Net] Change seed servers, and magic strings so as to avoid contact with the Bitcoin network.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,10 +121,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xf9;
-        pchMessageStart[1] = 0xbe;
-        pchMessageStart[2] = 0xb4;
-        pchMessageStart[3] = 0xd9;
+        pchMessageStart[0] = 0x2c;
+        pchMessageStart[1] = 0xfe;
+        pchMessageStart[2] = 0x7e;
+        pchMessageStart[3] = 0x6d;
         nDefaultPort = 8639;
         nPruneAfterHeight = 100000;
 
@@ -134,12 +134,9 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0xf53b1baa971ea40be88cf51288aabd700dfec96c486bf7155a53a4919af4c8bd"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.push_back(CDNSSeedData("bitcoin.sipa.be", "seed.bitcoin.sipa.be", true)); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.push_back(CDNSSeedData("bluematt.me", "dnsseed.bluematt.me", true)); // Matt Corallo, only supports x9
-        vSeeds.push_back(CDNSSeedData("dashjr.org", "dnsseed.freicoin.dashjr.org")); // Luke Dashjr
-        vSeeds.push_back(CDNSSeedData("freicoinstats.com", "seed.freicoinstats.com", true)); // Christian Decker, supports x1 - xf
-        vSeeds.push_back(CDNSSeedData("xf2.org", "bitseed.xf2.org")); // Jeff Garzik
-        vSeeds.push_back(CDNSSeedData("freicoin.jonasschnelli.ch", "seed.freicoin.jonasschnelli.ch", true)); // Jonas Schnelli, only supports x1, x5, x9, and xd
+        vSeeds.push_back(CDNSSeedData("node.freico.in", "seed.freico.in")); // Mark Friedenbach
+        vSeeds.push_back(CDNSSeedData("abacus.freico.in", "fledge.freico.in")); // @galambo
+        vSeeds.push_back(CDNSSeedData("seed.sicanet.net", "dnsseed.sicanet.net")); // Fredrik Bodin
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
@@ -250,10 +247,10 @@ public:
         consensus.filtered_adjust_interval = 9; // 1.5 hrs
         consensus.diff_adjust_threshold = 2016;
 
-        pchMessageStart[0] = 0x0b;
-        pchMessageStart[1] = 0x11;
-        pchMessageStart[2] = 0x09;
-        pchMessageStart[3] = 0x07;
+        pchMessageStart[0] = 0x5e;
+        pchMessageStart[1] = 0xd6;
+        pchMessageStart[2] = 0x7c;
+        pchMessageStart[3] = 0xf3;
         nDefaultPort = 18639;
         nPruneAfterHeight = 1000;
 
@@ -265,10 +262,6 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.push_back(CDNSSeedData("testnetfreicoin.jonasschnelli.ch", "testnet-seed.freicoin.jonasschnelli.ch", true));
-        vSeeds.push_back(CDNSSeedData("petertodd.org", "seed.tfrc.petertodd.org", true));
-        vSeeds.push_back(CDNSSeedData("bluematt.me", "testnet-seed.bluematt.me"));
-        vSeeds.push_back(CDNSSeedData("freicoin.schildbach.de", "testnet-seed.freicoin.schildbach.de"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
@@ -344,10 +337,10 @@ public:
         consensus.filtered_adjust_interval = 9; // 1.5 hrs
         consensus.diff_adjust_threshold = std::numeric_limits<int64_t>::max();
 
-        pchMessageStart[0] = 0xfa;
-        pchMessageStart[1] = 0xbf;
-        pchMessageStart[2] = 0xb5;
-        pchMessageStart[3] = 0xda;
+        pchMessageStart[0] = 0xed;
+        pchMessageStart[1] = 0x99;
+        pchMessageStart[2] = 0x9c;
+        pchMessageStart[3] = 0xf6;
         nDefaultPort = 28639;
         nPruneAfterHeight = 1000;
 
