a042985846776986752c9f0b7219f124ed931b2e
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:12:54 -0700

    [Net] Change network port, and all references to it.

--- src/netbase.h
+++ src/netbase.h
@@ -198,7 +198,7 @@ std::optional<CNetAddr> LookupHost(const std::string& name, bool fAllowLookup, D
  * @param name    The string representing a service. Could be a name or a
  *                numerical IP address (IPv6 addresses should be in their
  *                disambiguated bracketed form), optionally followed by a uint16_t port
- *                number. (e.g. example.com:8333 or
+ *                number. (e.g. example.com:8639 or
  *                [2001:db8:85a3:8d3:1319:8a2e:370:7348]:420)
  * @param portDefault The default port for resulting services if not specified
  *                    by the service string.
