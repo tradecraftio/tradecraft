e78c6468f6deb67b97b75ab39878ac87f0b2dbce
Mark Friedenbach <mark@friedenbach.org>
2013-08-06 13:51:38 -0700

    [Subsidy] Change MAX_MONEY to 2^53-1.

--- test/functional/test_framework/messages.py
+++ test/functional/test_framework/messages.py
@@ -531,7 +531,7 @@ class CTransaction:
     def is_valid(self):
         self.calc_sha256()
         for tout in self.vout:
-            if tout.nValue < 0 or tout.nValue > 21000000 * COIN:
+            if tout.nValue < 0 or tout.nValue > 9007199254740991:
                 return False
         return True
 
