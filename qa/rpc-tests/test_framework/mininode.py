a1e94a0afe053cbf825a0f50a15fbe63501893b9
Mark Friedenbach <mark@friedenbach.org>
2013-09-29 16:51:21 -0700

    [Refheight] Make nVersion=2 transactions the default in unit tests as well.

--- qa/rpc-tests/test_framework/mininode.py
+++ qa/rpc-tests/test_framework/mininode.py
@@ -434,7 +434,7 @@ class CTxWitness(object):
 class CTransaction(object):
     def __init__(self, tx=None):
         if tx is None:
-            self.nVersion = 1
+            self.nVersion = 2
             self.vin = []
             self.vout = []
             self.wit = CTxWitness()
