935dc882a1e97af5705c9bc6d0091c9e62be02ed
Mark Friedenbach <mark@friedenbach.org>
2016-08-15 11:28:52 -0700

    [Demurrage] As part of the nVersion=3 DER (BIP66) soft-fork, fractional amounts were truncated on a per-input basis. Prior to activation of that soft-fork, an additional value of one kria per input is included in order to prevent historical transactions from failing these new validation rules.

--- src/main.h
+++ src/main.h
@@ -328,7 +328,7 @@ unsigned int GetP2SHSigOpCount(const CTransaction& tx, const CCoinsViewCache& ma
  * This does not modify the UTXO set. If pvChecks is not NULL, script checks are pushed onto it
  * instead of being performed inline.
  */
-bool CheckInputs(const CTransaction& tx, CValidationState &state, const CCoinsViewCache &view, bool fScriptChecks,
+bool CheckInputs(const CTransaction& tx, CValidationState &state, const CCoinsViewCache &view, int per_input_adjustment, bool fScriptChecks,
                  unsigned int flags, bool cacheStore, std::vector<CScriptCheck> *pvChecks = NULL);
 
 /** Apply the effects of this transaction on the UTXO set represented by view */
