c7be9e99429af57332d8e68fd3cd3867096bd991
Mark Friedenbach <mark@friedenbach.org>
2018-03-28 12:21:34 -0700

    [Demurrage] Add 3.2% demurrage using an ALU implementation of TimeAdjustValueForward.
    Changes TimeAdjustValueForward to use an integer-math-only method for performing demurrage calculations.  Note that due to truncation of the intermediate multiplication terms it is possible for this method to disagree with the results of the previously used MPFR calculation by as much as 1 kria, even after the latter is truncated.  It boasts a 10x improvement in speed, however.

--- src/wallet/test/wallet_tests.cpp
+++ src/wallet/test/wallet_tests.cpp
@@ -156,7 +156,7 @@ BOOST_FIXTURE_TEST_CASE(scan_for_wallet_transactions, TestChain100Setup)
         BOOST_CHECK(result.last_failed_block.IsNull());
         BOOST_CHECK_EQUAL(result.last_scanned_block, newTip->GetBlockHash());
         BOOST_CHECK_EQUAL(*result.last_scanned_height, newTip->nHeight);
-        BOOST_CHECK_EQUAL(GetBalance(wallet).m_mine_immature, 100 * COIN);
+        BOOST_CHECK_EQUAL(GetBalance(wallet).m_mine_immature, GetTimeAdjustedValue(50 * COIN, 2) + GetTimeAdjustedValue(50 * COIN, 1));
 
         {
             CBlockLocator locator;
@@ -192,7 +192,7 @@ BOOST_FIXTURE_TEST_CASE(scan_for_wallet_transactions, TestChain100Setup)
         BOOST_CHECK_EQUAL(result.last_failed_block, oldTip->GetBlockHash());
         BOOST_CHECK_EQUAL(result.last_scanned_block, newTip->GetBlockHash());
         BOOST_CHECK_EQUAL(*result.last_scanned_height, newTip->nHeight);
-        BOOST_CHECK_EQUAL(GetBalance(wallet).m_mine_immature, 50 * COIN);
+        BOOST_CHECK_EQUAL(GetBalance(wallet).m_mine_immature, GetTimeAdjustedValue(50 * COIN, 1));
     }
 
     // Prune the remaining block file.
@@ -595,7 +595,7 @@ BOOST_FIXTURE_TEST_CASE(ListCoinsTest, ListCoinsTestingSetup)
     BOOST_CHECK_EQUAL(list.begin()->second.size(), 1U);
 
     // Check initial balance from one mature coinbase transaction.
-    BOOST_CHECK_EQUAL(50 * COIN, GetAvailableBalance(*wallet));
+    BOOST_CHECK_EQUAL(GetTimeAdjustedValue(50 * COIN, 101), GetAvailableBalance(*wallet));
 
     // Add a transaction creating a change address, and confirm ListCoins still
     // returns the coin associated with the change address underneath the
