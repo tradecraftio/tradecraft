074d8d6e6437d47001bfc2597fcd5e179654967c
Mark Friedenbach <mark@friedenbach.org>
2018-04-25 11:16:50 -0700

    [Refheight] Require lock_heights be less than or equal to the current block height.

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -442,6 +442,10 @@ bool CWallet::SelectCoins(const std::vector<COutput>& vAvailableCoins, const CAm
             if (wtx.tx->vout.size() <= outpoint.n) {
                 return false;
             }
+            // Cannot spend an input with refheight higher than our lock_height
+            if (wtx.tx->lock_height > height) {
+                return false;
+            }
             // Just to calculate the marginal byte size
             CInputCoin coin(height, wtx.tx, outpoint.n, wtx.GetSpendSize(outpoint.n, false));
             nValueFromPresetInputs += coin.txout.nValue;
