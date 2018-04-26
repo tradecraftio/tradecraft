a90491acd1b81c6dd7ff0c8a1d281d4352a888da
Mark Friedenbach <mark@friedenbach.org>
2018-04-25 18:01:09 -0700

    [Subsidy] Remove Bitcoin's inflation schedule, and replace with Freicoin's transient initial distribution and demurrage-compensating perpetual mining reward.

--- test/functional/p2p_invalid_block.py
+++ test/functional/p2p_invalid_block.py
@@ -114,7 +114,7 @@ class InvalidBlockRequestTest(FreicoinTestFramework):
 
         block3 = create_block(tip, create_coinbase(height), block_time)
         block_time += 1
-        block3.vtx[0].vout[0].nValue = 100 * COIN  # Too high!
+        block3.vtx[0].vout[0].nValue = 1000000 * COIN # Too high!
         block3.vtx[0].sha256 = None
         block3.vtx[0].calc_sha256()
         block3.hashMerkleRoot = block3.calc_merkle_root()
