084dcf7ff20dae2f9e0e30bdc3bd8a9d571d319e
Mark Friedenbach <mark@friedenbach.org>
2018-07-17 15:10:59 -0700

    [Mining] Revert default block size increase, back to demonstrably safe levels.

--- src/main.h
+++ src/main.h
@@ -62,10 +62,10 @@ class CValidationState;
 struct CNodeStateStats;
 
 /** Default for -blockmaxsize and -blockminsize, which control the range of sizes the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 750000;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 250000;
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
-static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
+static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 25000;
 /** Default for accepting alerts from the P2P network. */
 static const bool DEFAULT_ALERTS = false;
 /** The maximum size for transactions we're willing to relay/mine */
