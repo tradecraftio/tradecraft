ff9d7caa962537aac9dfe586a6cafb660c7510e2
Mark Friedenbach <mark@friedenbach.org>
2018-07-17 15:09:46 -0700

    [Data] Do not relay transactions with data outputs by default, which serve no purpose on a public block chain.

--- test/functional/mempool_dust.py
+++ test/functional/mempool_dust.py
@@ -51,7 +51,7 @@ DUST_RELAY_TX_FEE = 3000  # default setting [sat/kvB]
 class DustRelayFeeTest(FreicoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
-        self.extra_args = [['-permitbaremultisig']]
+        self.extra_args = [['-datacarrier=1', '-permitbaremultisig']]
 
     def test_dust_output(self, node: TestNode, dust_relay_fee: Decimal,
                          output_script: CScript, type_desc: str) -> None:
@@ -113,7 +113,7 @@ class DustRelayFeeTest(FreicoinTestFramework):
             else:
                 dust_parameter = f"-dustrelayfee={dustfee_frc_kvb:.8f}"
                 self.log.info(f"Test dust limit setting {dust_parameter} ({dustfee_sat_kvb} sat/kvB)...")
-                self.restart_node(0, extra_args=[dust_parameter, "-permitbaremultisig"])
+                self.restart_node(0, extra_args=[dust_parameter, "-datacarrier=1", "-permitbaremultisig"])
 
             for output_script, description in output_scripts:
                 self.test_dust_output(self.nodes[0], dustfee_frc_kvb, output_script, description)
