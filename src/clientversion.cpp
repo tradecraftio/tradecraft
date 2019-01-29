493dc1f743c63c3b258317bc71990327d30e0a1f
Mark Friedenbach <mark@friedenbach.org>
2019-01-29 06:58:05 -0800

    [Version] Switch to Freicoin/Tradecraft versioning scheme for version strings.
    In comparison to bitcoin release numbers, we promote minor version numbers to major, so for example release "0.10.4" becomes "10.4"; we repurpose the revision number as a subproject release number, so "10.4.1" is the second release based of upstream "0.10.4"; and we repurpose the build number as a monotonically increasing release number.

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -63,13 +63,13 @@ const std::string CLIENT_NAME("Satoshi");
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-" DO_STRINGIZE(suffix)
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "-" DO_STRINGIZE(build) "-" DO_STRINGIZE(suffix)
 
 #define BUILD_DESC_FROM_COMMIT(maj, min, rev, build, commit) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "-" DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "-" DO_STRINGIZE(build) "-unk"
 
 #ifndef BUILD_DESC
 #ifdef BUILD_SUFFIX
@@ -94,6 +94,10 @@ const std::string CLIENT_DATE(BUILD_DATE);
 
 static std::string FormatVersion(int nVersion)
 {
+    if (nVersion % 1000000 == 0)
+        return strprintf("%d", nVersion / 1000000);
+    if (nVersion % 10000 == 0)
+        return strprintf("%d.%d", nVersion / 1000000, (nVersion / 10000) % 100);
     if (nVersion % 100 == 0)
         return strprintf("%d.%d.%d", nVersion / 1000000, (nVersion / 10000) % 100, (nVersion / 100) % 100);
     else
