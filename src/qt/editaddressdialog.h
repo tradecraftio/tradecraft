275f2c0d399db0dffbc7752a7bc39c90b6d55d96
Mark Friedenbach <mark@friedenbach.org>
2019-02-12 15:45:14 -0800

    [License] Update copyright declaration for code files modified in later commits.

--- src/qt/editaddressdialog.h
+++ src/qt/editaddressdialog.h
@@ -1,6 +1,20 @@
 // Copyright (c) 2011-2013 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2011-2019 The Freicoin developers
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the conjunctive terms of BOTH version 3 of the GNU
+// Affero General Public License as published by the Free Software
+// Foundation AND the MIT/X11 software license.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License and the MIT/X11 software license for
+// more details.
+//
+// You should have received a copy of both licenses along with this
+// program.  If not, see <https://www.gnu.org/licenses/> and
+// <http://www.opensource.org/licenses/mit-license.php>
 
 #ifndef BITCOIN_QT_EDITADDRESSDIALOG_H
 #define BITCOIN_QT_EDITADDRESSDIALOG_H
--- src/secp256k1/src/field_5x52_asm_impl.h
+++ src/secp256k1/src/field_5x52_asm_impl.h
@@ -1,7 +1,21 @@
 /**********************************************************************
  * Copyright (c) 2013 Pieter Wuille                                   *
- * Distributed under the MIT software license, see the accompanying   *
- * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ * Copyright (c) 2018-2019 The Freicoin developers                    *
+ *                                                                    *
+ * This program is free software: you can redistribute it and/or      *
+ * modify it under the conjunctive terms of BOTH version 3 of the GNU *
+ * Affero General Public License as published by the Free Software    *
+ * Foundation AND the MIT/X11 software license.                       *
+ *                                                                    *
+ * This program is distributed in the hope that it will be useful,    *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
+ * Affero General Public License and the MIT/X11 software license for *
+ * more details.                                                      *
+ *                                                                    *
+ * You should have received a copy of both licenses along with this   *
+ * program.  If not, see <https://www.gnu.org/licenses/> and          *
+ * <http://www.opensource.org/licenses/mit-license.php>               *
  **********************************************************************/
 
 #ifndef _SECP256K1_FIELD_INNER5X52_IMPL_H_
