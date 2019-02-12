ab347b7ede31799604ff9b39ef05e5d9444db0a0
Mark Friedenbach <mark@friedenbach.org>
2019-02-12 15:45:14 -0800

    [License] Update copyright declaration for code files modified in later commits.

--- src/crypto/rfc6979_hmac_sha256.cpp
+++ src/crypto/rfc6979_hmac_sha256.cpp
@@ -1,6 +1,20 @@
 // Copyright (c) 2014 The Bitcoin developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2011-2019 The Freicoin developers
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the conjunctive terms of BOTH version 3 of the GNU
+// Affero General Public License as published by the Free Software
+// Foundation AND the MIT/X11 software license.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License and the MIT/X11 software license for
+// more details.
+//
+// You should have received a copy of both licenses along with this
+// program.  If not, see <https://www.gnu.org/licenses/> and
+// <http://www.opensource.org/licenses/mit-license.php>
 
 #include "crypto/rfc6979_hmac_sha256.h"
 
