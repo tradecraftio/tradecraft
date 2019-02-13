d7cb30b2a5e977c4261293e5a33600ace16b4a8b
Mark Friedenbach <mark@friedenbach.org>
2019-02-13 09:22:55 -0800

    [Sigops] Bump MAX_P2SH_SIGOPS from 15 to 20, since valid redeem scripts can be constructed within 520 bytes which use that many signature operations.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -37,7 +37,7 @@ static const unsigned int DEFAULT_BLOCK_MAX_WEIGHT = 3000000;
 /** The maximum weight for transactions we're willing to relay/mine */
 static const unsigned int MAX_STANDARD_TX_WEIGHT = 400000;
 /** Maximum number of signature check operations in an IsStandard() P2SH script */
-static const unsigned int MAX_P2SH_SIGOPS = 15;
+static const unsigned int MAX_P2SH_SIGOPS = 20;
 /** The maximum number of sigops we're willing to relay/mine in a single tx */
 static const unsigned int MAX_STANDARD_TX_SIGOPS_COST = MAX_BLOCK_SIGOPS_COST/5;
 /** Default for -maxmempool, maximum megabytes of mempool memory usage */
