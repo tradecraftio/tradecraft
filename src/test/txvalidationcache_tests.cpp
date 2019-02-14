99fef276bf70f2c453cc50142366b108efcbf17d
Mark Friedenbach <mark@friedenbach.org>
2019-02-14 02:03:46 -0800

    [CLTV] Change CHECKLOCKTIMEVERIFY to pop its argumennt off the stack.

--- src/test/txvalidationcache_tests.cpp
+++ src/test/txvalidationcache_tests.cpp
@@ -263,6 +263,10 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup)
         ValidateCheckInputsForAllFlags(CTransaction(invalid_under_p2sh_tx), SCRIPT_VERIFY_P2SH, true);
     }
 
+    // The following tests no longer work with a CLTV that pops arguments off
+    // the stack.  In a later commit we remove SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY
+    // entirely, so we don't even bother fixing the test.
+#if 0
     // Test CHECKLOCKTIMEVERIFY
     {
         CMutableTransaction invalid_with_cltv_tx;
@@ -292,6 +296,7 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, TestChain100Setup)
         PrecomputedTransactionData txdata(invalid_with_cltv_tx);
         BOOST_CHECK(CheckInputs(CTransaction(invalid_with_cltv_tx), state, ::ChainstateActive().CoinsTip(), Params().GetConsensus(), 0, SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY, true, true, txdata, nullptr));
     }
+#endif
 
     // TEST CHECKSEQUENCEVERIFY
     {
