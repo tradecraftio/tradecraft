badba06e12e0ab93eb33461d485ef53931005a1d
Mark Friedenbach <mark@friedenbach.org>
2019-02-18 16:03:31 +0100

    [Hard-Fork] Implement time-activated "protocol cleanup" flag-day hard-fork, set to activate in April 2021.
    To achieve desired scaling limits, the forward blocks architecture will eventually trigger a hard-fork modification of the consensus rules, for the primary purposes of dropping enforcement of many aggregate block limits and altering the difficulty adjustment algorithm.
    
    This hard-fork will not activate until it is absolutely necessary for it to do so, at the point when real demand for additional shard space in aggregate across all forward block shard-chains exceeds the available space in the compatibility chain.  It is anticipated that this will not occur until many, many years into the future, when Freicoin/Tradecraft's usage exceeds even the levels of bitcoin usage ca. 2018.  However when it does eventually trigger, any node enforcing the old rules will be left behind.
    
    Since the rule changes for forward blocks have not been written yet and because this flag-day code doesn't know how to detect actual activation, we cannot have older clients enforce the new rules.  What is do instead is that any rule which we anticipate changes becomes simply unenforced after this activation time, and aggregate limits are set to the maximum values the software is able to support.  After the flag-day, older clients of at least version 10.4 will continue to receive blocks, but with only SPV security ("trust the most work") for the new protocol rules.  So starting with the release of 10.4, activation of forward blocks new scaling limits becomes a soft-fork, with the only concern being the forking off of pre-10.4 nodes upon activation.
    
    The protocol cleanup rule change is scheduled for activation on 2 April 2021 at midnight UTC.  This is 4PM PDT, 7PM EDT, and 9AM JST.  Since the activation time is median-time-past, it'll actually trigger about an hour after this wall-clock time.
    
    This date is chosen to be roughly 2 years after the expected release date of official binaries.  While the Freicoin developer team doesn't have the resources to provide strong ongoing support beyond emergency fixes, we nevertheless have an ideal goal of supporting release binaries for up to 2 years following the first release from that series.  Any release of a new series prior to the deployment of forward blocks should set this to be at least two years from the time of that release.  When forward blocks is deployed, this parameter should be set to the highest value used in prior releases, and becomes the earliest time at which the hard-fork rules can activate.

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -22,8 +22,22 @@
 
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
 static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 4000000;
-/** The maximum allowed weight for a block, see BIP 141 (network rule) */
+/** The maximum allowed weight for a block (network rule) */
 static const unsigned int MAX_BLOCK_WEIGHT = 4000000;
+/** The maximum size of a blk?????.dat file (since v10)
+ ** (post-cleanup network rule) */
+static const unsigned int PROTOCOL_CLEANUP_MAX_BLOCKFILE_SIZE = 0x7f000000; // (2048 - 16) MiB
+/** The maximum serialized block size is constrained by the need to
+ ** fit inside a block file, which has an additional 8 bytes of file
+ ** data per block. (post-cleanup consensus rule) */
+static const unsigned int PROTOCOL_CLEANUP_MAX_BLOCK_SERIALIZED_SIZE = 0x7efffff8; // PROTOCOL_CLEANUP_MAX_BLOCKFILE_SIZE - 8
+/** The actual hard block size limit post-activation of the protocol cleanup
+ ** rules is PROTOCOL_CLEANUP_MAX_BLOCKFILE_SIZE - 8, but this limit can't be
+ ** reached for any real block because classic blocks have a minimum size and
+ ** non-witness data has quadruple weight.  So we can still keep the post-fork
+ ** weight limit as a relatively round number in binary, for faster
+ ** calculations.  (post-cleanup consensus rule) */
+static const unsigned int PROTOCOL_CLEANUP_MAX_BLOCK_WEIGHT = 0x7f000000; // (2048 - 16) MiB
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
