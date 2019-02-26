5b5bc0568fec35f022e5891ae01e16de9f2136cb
Mark Friedenbach <mark@friedenbach.org>
2019-02-26 16:37:38 +0100

    [Soft-fork] Require the nTimeLock value of a coinbase transaction to be equal to the median of the past 11 block times.
    This is an old-style supermajority rollout with BIP8/BIP9-like version bits and BIP8-like activation-on-timeout semantics.  It's not strictly compatible with those BIPs though, as we didn't want to back port all the necessary state management code.  But miners using BIP8/BIP9 compatible version bit software were able to signal for this rollout by configuring their devices to signal bit #28 (the highest-order BIP8/BIP9 bit).  Bit 28 needs to be set during the activation period between April 2, 2019 and October 2, 2019 for a block to be considered valid.  This is residual condition from a pre-versionbits soft-fork deployment mechanism, and should not trigger the normal warnings about unexpected versionbits deployments.

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -63,6 +63,8 @@ struct Params {
     /** Soft-fork activations */
     int64_t truncate_inputs_activation_height;
     int64_t alu_activation_height;
+    int64_t verify_coinbase_lock_time_activation_height;
+    int64_t verify_coinbase_lock_time_timeout;
     /** Block height and hash at which BIP34 becomes active */
     int BIP34Height;
     uint256 BIP34Hash;
