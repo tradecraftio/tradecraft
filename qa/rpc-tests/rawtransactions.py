d1f5406de5096c3a8dffda2b12701c0aa8b7fb1c
Mark Friedenbach <mark@friedenbach.org>
2019-03-13 22:36:46 -0700

    [License] Update copyright declaration for code files modified in later commits.

--- qa/rpc-tests/rawtransactions.py
+++ qa/rpc-tests/rawtransactions.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python2
 # Copyright (c) 2014 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2010-2019 The Freicoin Developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the conjunctive terms of BOTH version 3 of the GNU Affero
+# General Public License as published by the Free Software Foundation
+# AND the MIT/X11 software license.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License and the MIT/X11 software license for
+# more details.
+#
+# You should have received a copy of both licenses along with this
+# program.  If not, see <https://www.gnu.org/licenses/> and
+# <http://www.opensource.org/licenses/mit-license.php>
 
 #
 # Test re-org scenarios with a mempool that contains transactions
