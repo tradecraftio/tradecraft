0b1cb452d645b5ac6d875ec5d96fbb7ea28a6aee
Mark Friedenbach <mark@friedenbach.org>
2019-03-21 00:26:14 -0700

    [CLTV] Disable activation logic and remove SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY from standard flags.
    With the activation of segwit script and forward blocks around the corner, it is preferable to save the unused NOP extension space for signaling version upgrades, shard endpoints for transfers, etc.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -290,7 +290,6 @@ BASE_SCRIPTS = [
     'wallet_encryption.py --legacy-wallet',
     'wallet_encryption.py --descriptors',
     'feature_dersig.py',
-    'feature_cltv.py',
     'rpc_uptime.py',
     'wallet_resendwallettransactions.py --legacy-wallet',
     'wallet_resendwallettransactions.py --descriptors',
