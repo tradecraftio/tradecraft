e7903117b1cd72b01e4586bed51eaf1cd1efc619
Mark Friedenbach <mark@friedenbach.org>
2019-03-21 00:06:24 -0700

    [CSV] Disable activation logic and remove SCRIPT_VERIFY_CHECKSEQUENCEVERIFY from standard flags.
    With the activation of segwit script and forward blocks around the corner, it is preferable to save the unused NOP extension space for signaling version upgrades, shard endpoints for transfers, etc.

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -95,7 +95,6 @@ static constexpr unsigned int STANDARD_SCRIPT_VERIFY_FLAGS{MANDATORY_SCRIPT_VERI
                                                              SCRIPT_VERIFY_MINIMALIF |
                                                              SCRIPT_VERIFY_NULLFAIL |
                                                              SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY |
-                                                             SCRIPT_VERIFY_CHECKSEQUENCEVERIFY |
                                                              SCRIPT_VERIFY_LOW_S |
                                                              SCRIPT_VERIFY_WITNESS |
                                                              SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM |
