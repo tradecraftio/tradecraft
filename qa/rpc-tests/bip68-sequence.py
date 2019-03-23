32cb38204a2f200750458b429c9f2a32de392dce
Mark Friedenbach <mark@friedenbach.org>
2019-03-23 15:47:41 -0700

    [BIP68] Remove requirement that tx.Version>=2 for sequence locks to be enforced.

--- qa/rpc-tests/bip68-sequence.py
+++ qa/rpc-tests/bip68-sequence.py
@@ -118,11 +118,16 @@ class BIP68Test(FreicoinTestFramework):
         else:
             assert(False)
 
-        # Setting the version back down to 1 should disable the sequence lock,
-        # so this should be accepted.
+        # Setting the version back down to 1 does not disable the
+        # sequence lock, unlike bitcoin, so this is still rejected.
         tx2.nVersion = 1
 
-        self.nodes[0].sendrawtransaction(ToHex(tx2))
+        try:
+            self.nodes[0].sendrawtransaction(ToHex(tx2))
+        except JSONRPCException as exp:
+            assert_equal(exp.error["message"], NOT_FINAL_ERROR)
+        else:
+            assert(False)
 
     # Calculate the median time past of a prior block ("confirmations" before
     # the current tip).
