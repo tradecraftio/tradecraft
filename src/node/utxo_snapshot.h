1cff7fc92b56fd80f512b6be22864e2899c86c3e
Mark Friedenbach <mark@friedenbach.org>
2019-04-22 14:29:14 -0700

    [FinalTx] Store block-final transaction hash in UTXO database.

--- src/node/utxo_snapshot.h
+++ src/node/utxo_snapshot.h
@@ -6,6 +6,7 @@
 #ifndef BITCOIN_NODE_UTXO_SNAPSHOT_H
 #define BITCOIN_NODE_UTXO_SNAPSHOT_H
 
+#include <coins.h>
 #include <uint256.h>
 #include <serialize.h>
 
@@ -19,6 +20,11 @@ public:
     //! UTXO set contained in this snapshot.
     uint256 m_base_blockhash;
 
+    //! The hash and number of spendable outputs of the previous
+    //! block's final transaction, if Consensus::DEPLOYMENT_FINALTX is
+    //! active.
+    BlockFinalTxEntry m_final_tx;
+
     //! The number of coins in the UTXO set contained in this snapshot. Used
     //! during snapshot load to estimate progress of UTXO set reconstruction.
     uint64_t m_coins_count = 0;
@@ -26,12 +32,71 @@ public:
     SnapshotMetadata() { }
     SnapshotMetadata(
         const uint256& base_blockhash,
+        const BlockFinalTxEntry& final_tx,
         uint64_t coins_count,
         unsigned int nchaintx) :
             m_base_blockhash(base_blockhash),
+            m_final_tx(final_tx),
             m_coins_count(coins_count) { }
 
-    SERIALIZE_METHODS(SnapshotMetadata, obj) { READWRITE(obj.m_base_blockhash, obj.m_coins_count); }
+    template <typename Stream>
+    inline void Serialize(Stream& s) const {
+        // The only extended-serialization flag currently used is bit 0, which
+        // if set indicates the presence of a non-null BlockFinalTxEntry field.
+        unsigned char flags = !m_final_tx.IsNull();
+        // The high-order bit of the base blockhash is used to signal the use of
+        // extended serialization.  For all block hashes this bit will be zero,
+        // so we can safely use it to convey information in the serialization
+        // format.
+        uint256 base_blockhash(m_base_blockhash);
+        if (flags) {
+            if (base_blockhash.data()[31] & 0x80) {
+                // Can never happen on a real chain.  Even regtest has a minimum
+                // difficulty that ensures the high-order bit is clear.
+                throw std::ios_base::failure("High bit of base block hash already set");
+            }
+            // Set the high-order bit of the hash to indicate extended
+            // serialization.
+            base_blockhash.data()[31] ^= 0x80;
+        }
+        ::Serialize(s, base_blockhash);
+        // Write the extended serialization fields.
+        if (flags) {
+            ::Serialize(s, flags);
+            if (flags & 0x01) {
+                ::Serialize(s, m_final_tx);
+            }
+        }
+        // Write the number of coins last.
+        ::Serialize(s, m_coins_count);
+    }
+
+    template <typename Stream>
+    inline void Unserialize(Stream& s) {
+        ::Unserialize(s, m_base_blockhash);
+        // Check for extended serialization fields, which are indicated by
+        // setting the high bit of the base blockhash.
+        if (m_base_blockhash.data()[31] & 0x80) {
+            m_base_blockhash.data()[31] ^= 0x80;
+            unsigned char flags = 0;
+            ::Unserialize(s, flags);
+            // Process each of the indicated extended serialization fields.
+            if (flags & 0x01) {
+                ::Unserialize(s, m_final_tx);
+                flags ^= 0x01;
+            }
+            // Unrecognized fields are an unrecoverable error.
+            if (flags) {
+                throw std::ios_base::failure("Unknown snapshot extended serialization fields");
+            }
+        }
+        ::Unserialize(s, m_coins_count);
+    }
+
+    template <typename Stream>
+    SnapshotMetadata(deserialize_type, Stream& s) {
+        Unserialize(s);
+    }
 };
 } // namespace node
 
