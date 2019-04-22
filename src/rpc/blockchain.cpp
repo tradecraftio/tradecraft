cf40118a118a292cb4f00ac3faf0643c2a607456
Mark Friedenbach <mark@friedenbach.org>
2019-04-22 14:24:52 -0700

    [FinalTx] Add BIP9 deployment logic for block-final transactions.
    The dates used are dummy placeholders, to be filled in with actual deployment dates in a later commit.

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1317,6 +1317,7 @@ UniValue DeploymentInfo(const CBlockIndex* blockindex, const ChainstateManager& 
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_DERSIG);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_CLTV);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_CSV);
+    SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_FINALTX);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_SEGWIT);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TESTDUMMY);
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TAPROOT);
