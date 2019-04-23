a981c74996790988b6a2e6c7a9ded6b74c12a391
Mark Friedenbach <mark@friedenbach.org>
2019-04-23 11:53:04 -0700

    [FinalTx] Store/restore block-final transaction hashes to/from the CBlockUndo data structure.
    This introduces the extended serialization format for block undo files, in a way similar to the extended witness serialization format of segwit.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1877,6 +1877,8 @@ DisconnectResult Chainstate::DisconnectBlock(const CBlock& block, const CBlockIn
         }
     }
 
+    // restore the previous block-final transaction hash
+    view.SetFinalTx(blockUndo.final_tx);
     // move best block pointer to prevout block
     view.SetBestBlock(pindex->pprev->GetBlockHash());
 
@@ -2159,6 +2161,7 @@ bool Chainstate::ConnectBlock(const CBlock& block, BlockValidationState& state, 
     LogPrint(BCLog::BENCH, "    - Fork checks: %.2fms [%.2fs (%.2fms/blk)]\n", MILLI * (nTime2 - nTime1), nTimeForks * MICRO, nTimeForks * MILLI / nBlocksTotal);
 
     CBlockUndo blockundo;
+    blockundo.final_tx = view.GetFinalTx();
 
     // Precomputed transaction data pointers must not be invalidated
     // until after `control` has run the script checks (potentially
