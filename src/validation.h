e27c2cb00dd0c8bbc86fa8275e7ca9d415cf4b51
Mark Friedenbach <mark@friedenbach.org>
2019-04-23 11:54:58 -0700

    [FinalTx] Block-final transaction consensus-critical validation logic.
    Ensure that the chain of block-final transactions is created and maintained indefinitely by creating the initial output to be used, and ensuring that each block-final transaction leaves output(s) for the next block's miners to use. A minimal number of additional DoS protection rules are also checked.

--- src/validation.h
+++ src/validation.h
@@ -412,6 +412,9 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
 /** Check a block is completely valid from start to finish (only works on top of our current best block, with cs_main held) */
 bool TestBlockValidity(CValidationState& state, const CChainParams& chainparams, const CBlock& block, CBlockIndex* pindexPrev, bool fCheckPOW = true, bool fCheckMerkleRoot = true);
 
+/** Check whether block-final transaction rules are enforced for block. */
+bool IsFinalTxEnforced(const CBlockIndex* pindexPrev, const Consensus::Params& params);
+
 /** Check whether witness commitments are required for block. */
 bool IsWitnessEnabled(const CBlockIndex* pindexPrev, const Consensus::Params& params);
 
