88013345a3f68b94e0835cf7b4437822a06eddc6
Mark Friedenbach <mark@friedenbach.org>
2019-07-25 18:54:37 -0700

    [FinalTx] Add RPC tests of block-final transaction soft-fork activation.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -197,6 +197,7 @@ BASE_SCRIPTS = [
     'p2p_disconnect_ban.py',
     'rpc_decodescript.py',
     'rpc_blockchain.py',
+    'feature_block_final_tx.py',
     'rpc_deprecated.py',
     'wallet_disable.py',
     'wallet_change_address.py --legacy-wallet',
