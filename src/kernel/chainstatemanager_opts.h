54ba6fd4a792eb17c4cd5c90594057d217739443
Mark Friedenbach <mark@friedenbach.org>
2019-08-11 10:21:54 -0700

    [Chainparams] Delay IBD threshold to be 14 days instead of 24 hours.
    A new node that just turns on will consider itself to be in IBD if the current time is more than a day after the latest block timestamp. This commit changes that value from 1 day to 2 weeks, as it has been known to happen that more than 24 hours pass without finding a block on the Freicoin network.

--- src/kernel/chainstatemanager_opts.h
+++ src/kernel/chainstatemanager_opts.h
@@ -33,7 +33,7 @@ class CChainParams;
 class ValidationSignals;
 
 static constexpr bool DEFAULT_CHECKPOINTS_ENABLED{true};
-static constexpr auto DEFAULT_MAX_TIP_AGE{24h};
+static constexpr auto DEFAULT_MAX_TIP_AGE{14 * 24h};
 
 namespace kernel {
 
