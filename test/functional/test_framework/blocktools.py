aced30a3db0255aaf2ad62e982f2c0c7cf147e84
Mark Friedenbach <mark@friedenbach.org>
2019-09-04 02:04:25 +0200

    [Segwit] Change segwit commitment to be trivailly-spent output rather than OP_RETURN.

--- test/functional/test_framework/blocktools.py
+++ test/functional/test_framework/blocktools.py
@@ -28,7 +28,6 @@ from .script import (
     OP_1,
     OP_CHECKMULTISIG,
     OP_CHECKSIG,
-    OP_RETURN,
     OP_TRUE,
     hash160,
 )
@@ -83,7 +82,7 @@ WITNESS_COMMITMENT_HEADER = b"\xaa\x21\xa9\xed"
 def get_witness_script(witness_root, witness_nonce):
     witness_commitment = uint256_from_str(hash256(ser_uint256(witness_root)+ser_uint256(witness_nonce)))
     output_data = WITNESS_COMMITMENT_HEADER + ser_uint256(witness_commitment)
-    return CScript([OP_RETURN, output_data])
+    return CScript([output_data])
 
 
 # According to BIP141, blocks with witness rules active must commit to the
@@ -97,7 +96,7 @@ def add_witness_commitment(block, nonce=0):
     block.vtx[0].wit.vtxinwit = [CTxInWitness()]
     block.vtx[0].wit.vtxinwit[0].scriptWitness.stack = [ser_uint256(witness_nonce)]
 
-    # witness commitment is the last OP_RETURN output in coinbase
+    # witness commitment is the last qualifying output in coinbase
     block.vtx[0].vout.append(CTxOut(0, get_witness_script(witness_root, witness_nonce)))
     block.vtx[0].rehash()
     block.hashMerkleRoot = block.calc_merkle_root()
