07066e794fd95a3947e975119099081af7d05a23
Mark Friedenbach <mark@friedenbach.org>
2019-10-21 17:23:10 +0900

    [Segwit] Remove disabled opcode check for segwit scripts (unrecognized opcodes still generate BAD_OPCODE if executed).

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -312,7 +312,7 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
             if (!protocol_cleanup && (sigversion == SIGVERSION_BASE) && (opcode > OP_16 && ++nOpCount > MAX_OPS_PER_SCRIPT))
                 return set_error(serror, SCRIPT_ERR_OP_COUNT);
 
-            if (!protocol_cleanup && (
+            if (!protocol_cleanup && (sigversion == SIGVERSION_BASE) && (
                 opcode == OP_CAT ||
                 opcode == OP_SUBSTR ||
                 opcode == OP_LEFT ||
