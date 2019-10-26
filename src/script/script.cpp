f75d1571e78066a3a69957dd6d65a701dc2170ae
Mark Friedenbach <mark@friedenbach.org>
2019-10-26 04:13:23 -0700

    [Segwit] Return OP_SHA1 to the list of unallocated opcodes for use in future script versions.

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -348,5 +348,6 @@ bool IsOpSuccess(const opcodetype& opcode)
     return opcode == 80 || opcode == 98 || (opcode >= 126 && opcode <= 129) ||
            (opcode >= 131 && opcode <= 134) || (opcode >= 137 && opcode <= 138) ||
            (opcode >= 141 && opcode <= 142) || (opcode >= 149 && opcode <= 153) ||
+           opcode == 167 ||
            (opcode >= 187 && opcode <= 254);
 }
