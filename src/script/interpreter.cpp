afc8fc276b05c93cb511a6d1103db020612a26cf
Mark Friedenbach <mark@friedenbach.org>
2019-10-26 03:44:37 -0700

    [Segwit] Enforce 32,767-element witness stack limit, before opcodes are executed.

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1547,6 +1547,11 @@ static bool VerifyWitnessProgram(const CScriptWitness& witness, int witversion, 
             return set_error(serror, SCRIPT_ERR_PUSH_SIZE);
     }
 
+    // Disallow more then MAX_WITNESS_STACK_SIZE elements in witness stack
+    if (stack.size() > MAX_WITNESS_STACK_SIZE) {
+        return set_error(serror, SCRIPT_ERR_STACK_SIZE);
+    }
+
     if (!EvalScript(stack, scriptPubKey, flags, checker, SIGVERSION_WITNESS_V0, serror)) {
         return false;
     }
