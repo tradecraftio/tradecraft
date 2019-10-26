100a95a6b04fc3fb7c9cb0ed34331a4a66905592
Mark Friedenbach <mark@friedenbach.org>
2019-10-26 00:52:48 -0700

    [Segwit] Remove implicit CLEANSTACK requirement for segwit scripts.

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1972,8 +1972,7 @@ static bool ExecuteWitnessScript(const Span<const valtype>& stack_span, const CS
     // Run the script interpreter.
     if (!EvalScript(stack, scriptPubKey, flags, checker, sigversion, execdata, serror)) return false;
 
-    // Scripts inside witness implicitly require cleanstack behaviour
-    if (stack.size() != 1) return set_error(serror, SCRIPT_ERR_CLEANSTACK);
+    if (stack.empty()) return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
     if (!CastToBool(stack.back())) return set_error(serror, SCRIPT_ERR_EVAL_FALSE);
     return true;
 }
