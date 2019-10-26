4a285eee84e58c2aed8a9c81fced0e111fff3904
Mark Friedenbach <mark@friedenbach.org>
2019-10-26 03:17:31 -0700

    [Segwit] Unrecognized witness lengths are treated as unknown segwit versions and return true.

--- src/script/solver.cpp
+++ src/script/solver.cpp
@@ -181,12 +181,9 @@ TxoutType Solver(const CScript& scriptPubKey, std::vector<std::vector<unsigned c
         if (scriptPubKey.IsPayToAnchor()) {
             return TxoutType::ANCHOR;
         }
-        if (witnessversion != 0) {
-            vSolutionsRet.push_back(std::vector<unsigned char>{(unsigned char)witnessversion});
-            vSolutionsRet.push_back(std::move(witnessprogram));
-            return TxoutType::WITNESS_UNKNOWN;
-        }
-        return TxoutType::NONSTANDARD;
+        vSolutionsRet.push_back(std::vector<unsigned char>{(unsigned char)witnessversion});
+        vSolutionsRet.push_back(std::move(witnessprogram));
+        return TxoutType::WITNESS_UNKNOWN;
     }
 
     // Provably prunable, data-carrying output
