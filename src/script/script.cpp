288068a27dc2b6600f4101e42d7a410cd534098b
Mark Friedenbach <mark@friedenbach.org>
2019-10-28 06:05:50 -0700

    [Segwit] Treat NOP1..NOP10 as "return true" in witness scripts.

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -348,7 +348,7 @@ bool IsOpSuccess(const opcodetype& opcode)
     return opcode == 80 || opcode == 98 || (opcode >= 126 && opcode <= 129) ||
            (opcode >= 131 && opcode <= 134) || (opcode >= 137 && opcode <= 138) ||
            (opcode >= 141 && opcode <= 142) || (opcode >= 149 && opcode <= 153) ||
-           opcode == 167 ||
+           opcode == 167 || opcode == 176 || (opcode >= 179 && opcode <= 185) ||
            (opcode >= 187 && opcode <= 254);
 }
 
