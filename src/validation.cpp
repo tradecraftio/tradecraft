310d043143c9bc83ab17344c9df3d4b8073a758e
Mark Friedenbach <mark@friedenbach.org>
2019-11-04 13:31:32 -0800

    [Segwit] NULLDUMMY should never activate on Tradecraft.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1904,11 +1904,6 @@ static unsigned int GetBlockScriptFlags(const CBlockIndex* pindex, const Consens
         flags |= SCRIPT_VERIFY_DERSIG;
     }
 
-    // Start enforcing BIP147 NULLDUMMY (activated simultaneously with segwit)
-    if (IsWitnessEnabled(pindex->pprev, consensusparams)) {
-        flags |= SCRIPT_VERIFY_NULLDUMMY;
-    }
-
     return flags;
 }
 
