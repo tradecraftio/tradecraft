2c6cc6f96045e6d890af0947fa24453f2249b757
Mark Friedenbach <mark@friedenbach.org>
2019-11-08 12:52:28 -0800

    [CSV] Remove SCRIPT_VERIFY_CHECKSEQUENCEVERIFY; CSV becomes active in segwit scripts only, and automatically.

--- src/test/txvalidationcache_tests.cpp
+++ src/test/txvalidationcache_tests.cpp
@@ -328,13 +328,13 @@ BOOST_FIXTURE_TEST_CASE(checkinputs_test, Dersig100Setup)
         vchSig.push_back((unsigned char)SIGHASH_ALL);
         invalid_with_csv_tx.vin[0].scriptSig = CScript() << vchSig << 101;
 
-        ValidateCheckInputsForAllFlags(CTransaction(invalid_with_csv_tx), SCRIPT_VERIFY_CHECKSEQUENCEVERIFY, true, m_node.chainman->ActiveChainstate().CoinsTip());
+        ValidateCheckInputsForAllFlags(CTransaction(invalid_with_csv_tx), 0, true, m_node.chainman->ActiveChainstate().CoinsTip());
 
         // Make it valid, and check again
         invalid_with_csv_tx.vin[0].scriptSig = CScript() << vchSig << 100;
         TxValidationState state;
         PrecomputedTransactionData txdata;
-        BOOST_CHECK(CheckInputScripts(CTransaction(invalid_with_csv_tx), state, &m_node.chainman->ActiveChainstate().CoinsTip(), Params().GetConsensus(), 0, SCRIPT_VERIFY_CHECKSEQUENCEVERIFY, true, true, txdata, nullptr));
+        BOOST_CHECK(CheckInputScripts(CTransaction(invalid_with_csv_tx), state, &m_node.chainman->ActiveChainstate().CoinsTip(), Params().GetConsensus(), 0, 0, true, true, txdata, nullptr));
     }
 
     // TODO: add tests for remaining script flags
