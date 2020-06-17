071ad93f8e6f2710189e683b54b10aae92c71389
Mark Friedenbach <mark@friedenbach.org>
2020-06-17 03:37:16 -0700

    [MergeMining] Add consensus verification of auxiliary proof-of-work data.

--- src/validation.h
+++ src/validation.h
@@ -626,7 +626,7 @@ bool TestBlockValidity(BlockValidationState& state,
 bool HasValidProofOfWork(const std::vector<CBlockHeader>& headers, const Consensus::Params& consensusParams);
 
 /** Check if a block has been mutated (with respect to its merkle root and witness commitments). */
-bool IsBlockMutated(const CBlock& block, bool check_witness_root);
+bool IsBlockMutated(const CBlock& block, const Consensus::Params& consensusParams, bool check_witness_root);
 
 /** Return the sum of the claimed work on a given set of headers. No verification of PoW is done. */
 arith_uint256 CalculateClaimedHeadersWork(const std::vector<CBlockHeader>& headers);
