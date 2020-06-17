6a6d50431a24c964e77cbf009d3ec1dd0207b8b5
Mark Friedenbach <mark@friedenbach.org>
2020-06-17 02:55:48 -0700

    [MergeMining] Add auxiliary proof-of-work fields to CBlockHeader.

--- src/version.h
+++ src/version.h
@@ -20,7 +20,8 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70016;
+static const int VERSION_MASK = 0x00ffffff;
+static const int PROTOCOL_VERSION = 70017;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -46,10 +47,15 @@ static const int SHORT_IDS_BLOCKS_VERSION = 70014;
 //! not banning for invalid compact blocks starts with this version
 static const int INVALID_CB_NO_BAN_VERSION = 70015;
 
+//! serialization of CBlockHeader and CBlock may include auxiliary proof-of-work
+//! data starting with this version
+static const int AUX_POW_VERSION = 70016;
+
 //! "wtxidrelay" command for wtxid-based relay starts with this version
-static const int WTXID_RELAY_VERSION = 70016;
+static const int WTXID_RELAY_VERSION = 70017;
 
 // Make sure that none of the values above collide with
-// `SERIALIZE_TRANSACTION_NO_WITNESS` or `ADDRV2_FORMAT`.
+// `SERIALIZE_TRANSACTION_NO_WITNESS`, `SERIALIZE_BLOCK_NO_AUX_POW` or
+// `ADDRV2_FORMAT`.
 
 #endif // FREICOIN_VERSION_H
