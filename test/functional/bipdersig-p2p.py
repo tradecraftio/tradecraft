5cee8bed2ae53c5720ae3137fb8ea283789c275a
Mark Friedenbach <mark@friedenbach.org>
2020-06-17 03:25:17 -0700

    [MergeMining] Reject merge-mined blocks prior to activation, and non-merged-mined after.

--- test/functional/bipdersig-p2p.py
+++ test/functional/bipdersig-p2p.py
@@ -61,7 +61,7 @@ def create_transaction(node, coinbase, to_address, amount):
 class BIP66Test(FreicoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
-        self.extra_args = [['-promiscuousmempoolflags=1', '-whitelist=127.0.0.1']]
+        self.extra_args = [['-promiscuousmempoolflags=1', '-whitelist=127.0.0.1', '-vbparams=auxpow:0:0']]
         self.setup_clean_chain = True
 
     def run_test(self):
