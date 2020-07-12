e5720679f120fd98054920728164fbc03b02d7c2
Mark Friedenbach <mark@friedenbach.org>
2020-07-12 14:56:33 -0700

    [Testnet] Remove minimum-difficulty blocks from testnet/regtest.
    With the coming change to auxiliary proof-of-work and merge-mining, the justification for maintaining a separate minimum-difficulty scheme for testnet doesn't make sense.  Stripping out this capability makes the surrounding code easier to reason about.

--- src/miner.cpp
+++ src/miner.cpp
@@ -46,10 +46,6 @@ int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParam
     if (nOldTime < nNewTime)
         pblock->nTime = nNewTime;
 
-    // Updating time can change work required on testnet:
-    if (consensusParams.fPowAllowMinDifficultyBlocks)
-        pblock->nBits = GetNextWorkRequired(pindexPrev, pblock, consensusParams, pindexPrev ? GetActiveRules(consensusParams, *pindexPrev) : 0);
-
     return nNewTime - nOldTime;
 }
 
