5da7ae64035a2cad7b98b09ea4aaafaa82715e5a
Mark Friedenbach <mark@friedenbach.org>
2020-08-23 11:27:04 -0700

    [License] Switch stratum subsystem to the Mozilla Public License 2.0

--- src/stratum.cpp
+++ src/stratum.cpp
@@ -1,19 +1,7 @@
 // Copyright (c) 2020 The Freicoin Developers
-//
-// This program is free software: you can redistribute it and/or
-// modify it under the conjunctive terms of BOTH version 3 of the GNU
-// Affero General Public License as published by the Free Software
-// Foundation AND the MIT/X11 software license.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Affero General Public License and the MIT/X11 software license for
-// more details.
-//
-// You should have received a copy of both licenses along with this
-// program.  If not, see <https://www.gnu.org/licenses/> and
-// <http://www.opensource.org/licenses/mit-license.php>
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "stratum.h"
 
