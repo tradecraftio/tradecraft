f467b7cf08efe1274945249cf9198e2607154ad9
Mark Friedenbach <mark@friedenbach.org>
2020-10-13 17:54:57 -0700

    [MergeMine] Add initial merge mining connection manager, based on the stratum server.

--- src/stratum.h
+++ src/stratum.h
@@ -6,6 +6,18 @@
 #define BITCOIN_STRATUM_H
 
 #include <node/context.h>
+#include <uint256.h>
+
+#include <univalue.h>
+
+#include <stdint.h>
+
+#include <string>
+
+/** JSON ser/deser utility functions. */
+std::string HexInt4(uint32_t val);
+uint32_t ParseHexInt4(const UniValue& hex, const std::string& name);
+uint256 ParseUInt256(const UniValue& hex, const std::string& name);
 
 /** The minimum difficulty for stratum mining clients.  A difficulty setting of 10^3 would take a 1Thps miner ~4 seconds to find a share. */
 const double DEFAULT_MINING_DIFFICULTY = 1e3;
