36934042119145954e785236b72dbdc4b4b3866c
Mark Friedenbach <mark@friedenbach.org>
2020-10-13 17:54:57 -0700

    [MergeMine] Add initial merge mining connection manager, based on the stratum server.

--- src/rpc/register.h
+++ src/rpc/register.h
@@ -18,6 +18,7 @@ void RegisterMiningRPCCommands(CRPCTable &tableRPC);
 void RegisterNodeRPCCommands(CRPCTable&);
 void RegisterNetRPCCommands(CRPCTable&);
 void RegisterOutputScriptRPCCommands(CRPCTable&);
+void RegisterMergeMineRPCCommands(CRPCTable&);
 void RegisterRawTransactionRPCCommands(CRPCTable &tableRPC);
 void RegisterSignMessageRPCCommands(CRPCTable&);
 void RegisterSignerRPCCommands(CRPCTable &tableRPC);
@@ -33,6 +34,7 @@ static inline void RegisterAllCoreRPCCommands(CRPCTable &t)
     RegisterNodeRPCCommands(t);
     RegisterNetRPCCommands(t);
     RegisterOutputScriptRPCCommands(t);
+    RegisterMergeMineRPCCommands(t);
     RegisterRawTransactionRPCCommands(t);
     RegisterSignMessageRPCCommands(t);
 #ifdef ENABLE_EXTERNAL_SIGNER
