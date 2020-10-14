34508e226788008383325abafa5624892a824cd1
Mark Friedenbach <mark@friedenbach.org>
2020-10-13 17:54:57 -0700

    [MergeMine] Add initial merge mining connection manager, based on the stratum server.

--- src/logging.cpp
+++ src/logging.cpp
@@ -155,6 +155,7 @@ const CLogCategoryDesc LogCategories[] =
     {BCLog::QT, "qt"},
     {BCLog::LEVELDB, "leveldb"},
     {BCLog::VALIDATION, "validation"},
+    {BCLog::MERGEMINE, "mergemine"},
     {BCLog::STRATUM, "stratum"},
     {BCLog::ALL, "1"},
     {BCLog::ALL, "all"},
