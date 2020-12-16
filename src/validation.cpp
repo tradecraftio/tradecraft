6431fd43eb43485ca79bfde9c4f709b9ac8aa2f8
Mark Friedenbach <mark@friedenbach.org>
2020-12-16 14:59:23 -0800

    [PoW] Allow nBits field to be biased in CheckProofOfWork validation.
    To check a merge-mined header for validity, we need to be able to calculated the proper target according to the merge-mining rules, which allows the miner to offset or 'bias' the difficulty target by a power-of-two.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3674,7 +3674,7 @@ void Chainstate::ReceivedBlockTransactions(const CBlock& block, CBlockIndex* pin
 static bool CheckBlockHeader(const CBlockHeader& block, BlockValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW = true)
 {
     // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
+    if (fCheckPOW && !CheckProofOfWork(block.GetHash(), block.nBits, 0, consensusParams))
         return state.Invalid(BlockValidationResult::BLOCK_INVALID_HEADER, "high-hash", "proof of work failed");
 
     return true;
@@ -3796,7 +3796,7 @@ std::vector<unsigned char> ChainstateManager::GenerateCoinbaseCommitment(CBlock&
 bool HasValidProofOfWork(const std::vector<CBlockHeader>& headers, const Consensus::Params& consensusParams)
 {
     return std::all_of(headers.cbegin(), headers.cend(),
-            [&](const auto& header) { return CheckProofOfWork(header.GetHash(), header.nBits, consensusParams);});
+            [&](const auto& header) { return CheckProofOfWork(header.GetHash(), header.nBits, 0, consensusParams);});
 }
 
 arith_uint256 CalculateHeadersWork(const std::vector<CBlockHeader>& headers)
