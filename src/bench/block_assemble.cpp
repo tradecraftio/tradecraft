69e11ddf667998d2098a4e681dd756fbcf903b72
Mark Friedenbach <mark@friedenbach.org>
2020-12-16 14:59:23 -0800

    [PoW] Allow nBits field to be biased in CheckProofOfWork validation.
    To check a merge-mined header for validity, we need to be able to calculated the proper target according to the merge-mining rules, which allows the miner to offset or 'bias' the difficulty target by a power-of-two.

--- src/bench/block_assemble.cpp
+++ src/bench/block_assemble.cpp
@@ -40,7 +40,7 @@ static CTxIn MineBlock(const CScript& coinbase_scriptPubKey)
 {
     auto block = PrepareBlock(coinbase_scriptPubKey);
 
-    while (!CheckProofOfWork(block->GetHash(), block->nBits, Params().GetConsensus())) {
+    while (!CheckProofOfWork(block->GetHash(), block->nBits, 0, Params().GetConsensus())) {
         assert(++block->nNonce);
     }
 
