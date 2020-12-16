ae6f034480c3cfd01c40e25e87b61b52b8b1a250
Mark Friedenbach <mark@friedenbach.org>
2020-12-16 14:59:23 -0800

    [PoW] Allow nBits field to be biased in CheckProofOfWork validation.
    To check a merge-mined header for validity, we need to be able to calculated the proper target according to the merge-mining rules, which allows the miner to offset or 'bias' the difficulty target by a power-of-two.

--- src/test/util/mining.cpp
+++ src/test/util/mining.cpp
@@ -52,7 +52,7 @@ std::vector<std::shared_ptr<CBlock>> CreateBlockChain(size_t total_height, const
         block.nBits = params.GenesisBlock().nBits;
         block.nNonce = 0;
 
-        while (!CheckProofOfWork(block.GetHash(), block.nBits, params.GetConsensus())) {
+        while (!CheckProofOfWork(block.GetHash(), block.nBits, 0, params.GetConsensus())) {
             ++block.nNonce;
             assert(block.nNonce);
         }
@@ -86,7 +86,7 @@ protected:
 
 COutPoint MineBlock(const NodeContext& node, std::shared_ptr<CBlock>& block)
 {
-    while (!CheckProofOfWork(block->GetHash(), block->nBits, Params().GetConsensus())) {
+    while (!CheckProofOfWork(block->GetHash(), block->nBits, 0, Params().GetConsensus())) {
         ++block->nNonce;
         assert(block->nNonce);
     }
