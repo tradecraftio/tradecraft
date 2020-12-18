9e4d3ff4d80f8490a3bc2982b64fb0d4efd86dcc
Mark Friedenbach <mark@friedenbach.org>
2020-12-18 04:09:52 -0800

    [MergeMine] Implement basic parent-chain merge mining protocol.

--- src/mergemine.h
+++ src/mergemine.h
@@ -10,14 +10,90 @@
 #include <sync.h>
 #include <uint256.h>
 
+#include <univalue.h>
+
 #include <map>
+#include <optional>
 #include <string>
+
+struct AuxWork {
+    uint64_t timestamp;
+    std::string job_id;
+    uint256 commit;
+    uint32_t bits;
+    unsigned char bias;
+
+    AuxWork() : timestamp(0), bits(0x1dffffff), bias(0) { }
+    AuxWork(uint64_t timestampIn, const std::string& job_idIn, const uint256& commitIn, uint32_t bitsIn, unsigned char biasIn) : timestamp(timestampIn), job_id(job_idIn), commit(commitIn), bits(bitsIn), bias(biasIn) { }
+};
+
+struct AuxProof {
+    uint256 midstate_hash;
+    std::vector<unsigned char> midstate_buffer;
+    uint32_t midstate_length;
+    uint32_t lock_time;
+    std::vector<uint256> aux_branch;
+    uint32_t num_txns;
+
+    uint32_t nVersion;
+    uint256 hashPrevBlock;
+    uint32_t nTime;
+    uint32_t nBits;
+    uint32_t nNonce;
+
+    AuxProof() : midstate_length(0), lock_time(0), num_txns(0), nVersion(0), nTime(0), nBits(0), nNonce(0) { }
+};
+
+struct SecondStageWork {
+    uint64_t timestamp;
+    double diff;
+    std::string job_id;
+    uint256 hashPrevBlock;
+    std::vector<unsigned char> cb1;
+    std::vector<unsigned char> cb2;
+    std::vector<uint256> cb_branch;
+    uint32_t nVersion;
+    uint32_t nBits;
+    uint32_t nTime;
+
+    SecondStageWork() : timestamp(0), diff(0), nVersion(0), nBits(0), nTime(0) { }
+    SecondStageWork(uint64_t timestampIn, double diffIn, const std::string& job_idIn, const uint256& hashPrevBlockIn, const std::vector<unsigned char>& cb1In, const std::vector<unsigned char>& cb2In, const std::vector<uint256>& cb_branchIn, uint32_t nVersionIn, uint32_t nBitsIn, uint32_t nTimeIn) : timestamp(timestampIn), diff(diffIn), job_id(job_idIn), hashPrevBlock(hashPrevBlockIn), cb1(cb1In), cb2(cb2In), cb_branch(cb_branchIn), nVersion(nVersionIn), nBits(nBitsIn), nTime(nTimeIn) { }
+};
+
+struct SecondStageProof {
+    std::vector<unsigned char> extranonce1;
+    std::vector<unsigned char> extranonce2;
+    uint32_t nVersion;
+    uint32_t nTime;
+    uint32_t nNonce;
+
+    SecondStageProof() : nVersion(0), nTime(0), nNonce(0) { }
+    SecondStageProof(const std::vector<unsigned char>& extranonce1In, const std::vector<unsigned char>& extranonce2In, uint32_t nVersionIn, uint32_t nTimeIn, uint32_t nNonceIn) : extranonce1(extranonce1In), extranonce2(extranonce2In), nVersion(nVersionIn), nTime(nTimeIn), nNonce(nNonceIn) { }
+};
 
 //! Mapping of alternative names to chain specifiers
 extern std::map<std::string, uint256> chain_names;
 
 //! Critical seciton guarding access to all of the merge-mining global state
 extern RecursiveMutex cs_merge_mining;
+
+/** Registers a mining client to a merge-mined chain, so that we will
+ ** see auxiliary notifications for that miner on that chain. */
+void RegisterMergeMineClient(const uint256& chainid, const std::string& username, const std::string& password);
+
+/** Takes a mapping of chainid -> username and returns a mapping of chainid -> AuxWork */
+std::map<uint256, AuxWork> GetMergeMineWork(const std::map<uint256, std::pair<std::string, std::string> >& auth);
+
+/** Returns a second stage work unit, if there is one available.  If the client
+ ** is currently working on a second-stage work unit, it is passed as a hint and
+ ** the merge-mining subsystem will return the same work if it is still
+ ** valid. */
+std::optional<std::pair<uint256, SecondStageWork> > GetSecondStageWork(std::optional<uint256> hint);
+
+/** Submit share to the specified auxiliary chain. */
+void SubmitAuxChainShare(const uint256& chainid, const std::string& username, const AuxWork& work, const AuxProof& proof);
+
+void SubmitSecondStageShare(const uint256& chainid, const std::string& username, const SecondStageWork& work, const SecondStageProof& proof);
 
 /** Reconnect to any auxiliary work sources with dropped connections. */
 void ReconnectToMergeMineEndpoints();
