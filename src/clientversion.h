111856cd3f59225dad06bfcf6e8fa4406d50cd4c
Mark Friedenbach <mark@friedenbach.org>
2021-01-13 13:26:13 -0800

    [Release] v13.2.5.1-11914.

--- src/clientversion.h
+++ src/clientversion.h
@@ -31,8 +31,8 @@
 #define CLIENT_VERSION_MAJOR 13
 #define CLIENT_VERSION_MINOR 2
 #define CLIENT_VERSION_REVISION 5
-#define CLIENT_VERSION_PATCH 0
-#define CLIENT_VERSION_BUILD 11909
+#define CLIENT_VERSION_PATCH 1
+#define CLIENT_VERSION_BUILD 11914
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
