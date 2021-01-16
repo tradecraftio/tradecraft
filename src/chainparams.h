c4a324de0fafdd2de2fe043dd8b7e68d15bb48e8
Mark Friedenbach <mark@friedenbach.org>
2021-01-15 23:54:36 -0800

    [MergeMine] Add DefaultAuxPowPath to chain parameters.
    Make the default aux_pow_path value a chain params option.

--- src/chainparams.h
+++ src/chainparams.h
@@ -77,6 +77,8 @@ public:
     const std::vector<SeedSpec6>& FixedSeeds() const { return vFixedSeeds; }
     const CCheckpointData& Checkpoints() const { return checkpointData; }
     const ChainTxData& TxData() const { return chainTxData; }
+    /** Default merge-mine chain if chain id isn't specified */
+    const uint256& DefaultAuxPowPath() const { return default_aux_pow_path; }
 protected:
     CChainParams() {}
 
@@ -95,6 +97,7 @@ protected:
     bool fMineBlocksOnDemand;
     CCheckpointData checkpointData;
     ChainTxData chainTxData;
+    uint256 default_aux_pow_path;
 };
 
 /**
