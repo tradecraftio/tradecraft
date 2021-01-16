5c6eeb65b1a0191cce0e0436e3eeedf3c76e3501
Mark Friedenbach <mark@friedenbach.org>
2021-01-15 23:54:36 -0800

    [MergeMine] Add DefaultAuxPowPath to chain parameters.
    Make the default aux_pow_path value a chain params option.

--- src/kernel/chainparams.h
+++ src/kernel/chainparams.h
@@ -74,6 +74,14 @@ struct ChainTxData {
 };
 
 /**
+ * Holds the auxiliary chain ID for a particular chain.  Used for merged mining.
+ */
+struct ChainId : BaseHash<uint256> {
+    ChainId() : BaseHash() {}
+    explicit ChainId(const uint256& hash) : BaseHash(hash) {}
+};
+
+/**
  * CChainParams defines various tweakable parameters of a given instance of the
  * Bitcoin system.
  */
@@ -130,6 +138,9 @@ public:
 
     const ChainTxData& TxData() const { return chainTxData; }
 
+    /** Default merge-mine chain if chain id isn't specified */
+    const ChainId& DefaultAuxPowPath() const { return default_aux_pow_path; }
+
     /**
      * SigNetOptions holds configurations for creating a signet CChainParams.
      */
@@ -181,6 +192,7 @@ protected:
     CCheckpointData checkpointData;
     std::vector<AssumeutxoData> m_assumeutxo_data;
     ChainTxData chainTxData;
+    ChainId default_aux_pow_path;
 };
 
 #endif // BITCOIN_KERNEL_CHAINPARAMS_H
