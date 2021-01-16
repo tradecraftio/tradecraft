8535a69c2142b8e3248342d508083fd44b681d67
Mark Friedenbach <mark@friedenbach.org>
2021-01-15 23:54:36 -0800

    [MergeMine] Add DefaultAuxPowPath to chain parameters.
    Make the default aux_pow_path value a chain params option.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -173,6 +173,9 @@ public:
             .nTxCount = 760120522,
             .dTxRate  = 2.925802860942233,
         };
+
+        /* The Tradecraft/Freicoin main net. */
+        default_aux_pow_path = ChainId(uint256S("0x632938ec752e63b7f63cdd9a16b336c6c5cefbaad66278e402ce59d706f57ff6"));
     }
 };
 
@@ -269,6 +272,9 @@ public:
             .nTxCount = 63531852,
             .dTxRate  = 0.1079119341520164,
         };
+
+        /* The Tradecraft/Freicoin test net. */
+        default_aux_pow_path = ChainId(uint256S("0xe99fc44bfacee2f7e28d135845ff8a385d6d31353928d5b499700f1a2ad1b18b"));
     }
 };
 
@@ -476,6 +482,9 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
         bech32_hrp = "bcrt";
+
+        /* The Tradecraft/Freicoin regtest network. */
+        default_aux_pow_path = ChainId(uint256S("0xd799d41af01c1ac77e6a7793ba046a7432bb6ec250b84e2f5c6f225e05f0fc74"));
     }
 
     /**
