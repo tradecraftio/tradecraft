542d18b27ac70f72b1dea4084d8a7e395424d04f
Mark Friedenbach <mark@friedenbach.org>
2021-05-12 12:41:19 -0700

    [MergeMining] Use auxiliary proof-of-work fields from chainstate database.
    Use auxiliary proof-of-work data from the chainstate database when loading blocks from disk, to work around a bug which is allowing blocks with missing auxiliary proof-of-work headers to be stored in block files.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -1063,6 +1063,25 @@ bool BlockManager::ReadBlockFromDisk(CBlock& block, const CBlockIndex& index) co
     if (!ReadBlockFromDisk(block, block_pos)) {
         return false;
     }
+
+    // The activation of auxiliary proof-of-work introduces some unavoidable
+    // malleability into the block serialization format.  A merge-mined block
+    // can be serialized with or without its auxiliary proof-of-work header, and
+    // the choice does not change the calculated block hash.  Through a process
+    // which is at this time not well characterized, it appears possible for
+    // nodes to get into a state in which the block file on disk is missing the
+    // auxiliary proof-of-work of a fully validated block, but the chainstate
+    // database (which contains the block headers in the form of CBlockIndex
+    // records) has the complete header with auxiliary proof-of-work fields.
+    //
+    // Until the underlying bug is found and fixed, the workaround is to copy
+    // the auxiliary proof-of-work fields from the chainstate database when
+    // loading a block from disk.  In tests this is shown to make the complete
+    // block header available wherever the block is used.
+    if (block.m_aux_pow.IsNull()) {
+        block.m_aux_pow = index.m_aux_pow;
+    }
+
     if (block.GetHash() != index.GetBlockHash()) {
         return error("ReadBlockFromDisk(CBlock&, CBlockIndex*): GetHash() doesn't match index for %s at %s",
                      index.ToString(), block_pos.ToString());
