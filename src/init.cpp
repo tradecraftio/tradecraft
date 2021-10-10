1a096cce711c2971ff4af6cac47ed38261fac25b
Mark Friedenbach <mark@friedenbach.org>
2021-10-10 04:01:52 -0700

    f '[Stratum] Add StratumPort to chain parameters.'
    Restrict unqualified -stratumport configuration option to mainnet.

--- src/init.cpp
+++ src/init.cpp
@@ -554,7 +554,7 @@ void SetupServerArgs()
 
     gArgs.AddArg("-stratum", "Enable stratum server (default: off)", ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
     gArgs.AddArg("-stratumbind=<addr>", "Bind to given address to listen for Stratum work requests. Use [host]:port notation for IPv6. This option can be specified multiple times (default: bind to all interfaces)", ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
-    gArgs.AddArg("-stratumport=<port>", strprintf("Listen for Stratum work requests on <port> (default: %u or testnet: %u)", defaultBaseParams->StratumPort(), testnetBaseParams->StratumPort()), ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
+    gArgs.AddArg("-stratumport=<port>", strprintf("Listen for Stratum work requests on <port> (default: %u or testnet: %u)", defaultBaseParams->StratumPort(), testnetBaseParams->StratumPort()), ArgsManager::ALLOW_ANY | ArgsManager::NETWORK_ONLY, OptionsCategory::STRATUM);
     gArgs.AddArg("-stratumallowip=<ip>", "Allow Stratum work requests from specified source. Valid for <ip> are a single IP (e.g. 1.2.3.4), a network/netmask (e.g. 1.2.3.4/255.255.255.0) or a network/CIDR (e.g. 1.2.3.4/24). This option can be specified multiple times", ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
     gArgs.AddArg("-mergemine=<addr>:<port>", "Merge-mine another chain using the auxiliary block commitment information served by stratum+tcp://<addr>:<port>", ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
     gArgs.AddArg("-mergeminename=<name>:<chainid>", "Use <name> as an alternative specifier for the given chainid.", ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
