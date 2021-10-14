e8b7354dc0d5300dd5ca7105f98e5e6364c87bc1
Mark Friedenbach <mark@friedenbach.org>
2021-10-13 19:41:08 -0700

    Revert "[wallet] Remove addwitnessaddress"
    This reverts commits ebec90ac9731f8955d120a5d8419948d500b31c9 and 07e3f585abcee10c0283f2bd2f50eaa953df4d41, and additionally reverts the depreciation of 'addwitnessaddress'.
    
    Unlike bitcoin, our segwit implementation supports an arbitrary hierarchy of MAST scripts, and 'addwitnessaddress' is currently the only way to add a MAST script which requires a proof to spend.  Until alternative RPCs are written, 'addwitnessaddress' will not be depreciated or removed.

--- src/wallet/rpc/wallet.cpp
+++ src/wallet/rpc/wallet.cpp
@@ -835,6 +835,7 @@ RPCHelpMan getrawchangeaddress();
 RPCHelpMan setlabel();
 RPCHelpMan listaddressgroupings();
 RPCHelpMan addmultisigaddress();
+RPCHelpMan addwitnessaddress();
 RPCHelpMan keypoolrefill();
 RPCHelpMan newkeypool();
 RPCHelpMan getaddressesbylabel();
@@ -907,6 +908,7 @@ Span<const CRPCCommand> GetWalletRPCCommands()
         {"wallet", &abandontransaction},
         {"wallet", &abortrescan},
         {"wallet", &addmultisigaddress},
+        {"wallet", &addwitnessaddress},
         {"wallet", &backupwallet},
         {"wallet", &bumpfee},
         {"wallet", &psbtbumpfee},
