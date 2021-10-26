d05499d478c7926c8d718cc435480ddf31b0f043
Mark Friedenbach <mark@friedenbach.org>
2021-10-25 21:56:52 -0700

    [Mining] Allow rpc.getblocktemplate() to be called without network connections on regtest.
    Previously a JSONRPCError would be thrown if getblocktemplate is called on a node that is not connected to the network.  This is a good safeguard for public networks, but is just an annoying restriction to work around in test code.  This commit removes that restriction for regtest, or any other network that allows mining of blocks on demand.

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -436,7 +436,7 @@ static UniValue getblocktemplate(const JSONRPCRequest& request)
     if(!g_connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    if (g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
+    if (!Params().MineBlocksOnDemand() && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, PACKAGE_NAME " is not connected!");
 
     if (::ChainstateActive().IsInitialBlockDownload())
