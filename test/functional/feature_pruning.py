85b4daf8ad39da9e221ab55a4294ab78b4318bba
Mark Friedenbach <mark@friedenbach.org>
2021-10-25 21:56:12 -0700

    f '[FinalTx] Add mining code to produce block-final transactions once activated.'
    Update feature_pruning.py to generate block-final transactions.

--- test/functional/feature_pruning.py
+++ test/functional/feature_pruning.py
@@ -10,8 +10,8 @@ This test takes 30 mins or more (up to 2 hours)
 """
 import os
 
-from test_framework.blocktools import create_coinbase
-from test_framework.messages import CBlock, ToHex
+from test_framework.blocktools import create_coinbase, get_final_tx_info, add_final_tx
+from test_framework.messages import CBlock, CTxOut, ToHex
 from test_framework.script import CScript, OP_RETURN, OP_NOP
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
@@ -42,6 +42,7 @@ def mine_large_blocks(node, n):
     # Get the block parameters for the first block
     big_script = CScript([OP_RETURN] + [OP_NOP] * 950000)
     best_block = node.getblock(node.getbestblockhash())
+    final_tx = []
     height = int(best_block["height"]) + 1
     mine_large_blocks.nTime = max(mine_large_blocks.nTime, int(best_block["time"])) + 1
     previousblockhash = int(best_block["hash"], 16)
@@ -50,7 +51,9 @@ def mine_large_blocks(node, n):
         # Build the coinbase transaction (with large scriptPubKey)
         coinbase_tx = create_coinbase(height)
         coinbase_tx.vin[0].nSequence = 2 ** 32 - 1
-        coinbase_tx.vout[0].scriptPubKey = big_script
+        # We keep the block reward in an anyone-can-spend output at
+        # coinbase_tx.vout[0]
+        coinbase_tx.vout.append(CTxOut(0, big_script))
         coinbase_tx.rehash()
 
         # Build the block
@@ -61,6 +64,10 @@ def mine_large_blocks(node, n):
         block.nBits = int('207fffff', 16)
         block.nNonce = 0
         block.vtx = [coinbase_tx]
+        if not final_tx:
+            final_tx = get_final_tx_info(node)
+        if final_tx:
+            final_tx = add_final_tx(final_tx, block)
         block.hashMerkleRoot = block.calc_merkle_root()
         block.solve()
 
