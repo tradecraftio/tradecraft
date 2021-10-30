42df286b30c4def1131bcbdbc5721625204851e0
Mark Friedenbach <mark@friedenbach.org>
2021-10-30 03:53:04 -0700

    Revert "[MergeMine] Add DefaultAuxPowPath to chain parameters."
    This reverts commit faf266073ed265d2c24ce8851e0a879b44f4ae6d.

--- src/chainparams.h
+++ src/chainparams.h
@@ -84,8 +84,6 @@ public:
     const std::vector<SeedSpec6>& FixedSeeds() const { return vFixedSeeds; }
     const CCheckpointData& Checkpoints() const { return checkpointData; }
     const ChainTxData& TxData() const { return chainTxData; }
-    /** Default merge-mine chain if chain id isn't specified */
-    const uint256& DefaultAuxPowPath() const { return default_aux_pow_path; }
 protected:
     CChainParams() {}
 
@@ -106,7 +104,6 @@ protected:
     bool m_is_test_chain;
     CCheckpointData checkpointData;
     ChainTxData chainTxData;
-    uint256 default_aux_pow_path;
 };
 
 /**
