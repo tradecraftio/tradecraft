cad4b32d8b3ce733a48a6ee297b671a53dbe36b8
Mark Friedenbach <mark@friedenbach.org>
2021-12-16 22:34:24 -0800

    Revert "[MergeMine] Add DefaultAuxPowPath to chain parameters."
    This reverts commit 03d41b639fc20535fae134ef396c2cd60cd25349.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -166,9 +166,6 @@ public:
             /* nTxCount */ 517186863,
             /* dTxRate  */ 3.305709665792344,
         };
-
-        /* The Tradecraft/Freicoin main net. */
-        default_aux_pow_path = uint256S("0x632938ec752e63b7f63cdd9a16b336c6c5cefbaad66278e402ce59d706f57ff6");
     }
 };
 
@@ -259,9 +256,6 @@ public:
             /* nTxCount */ 13483,
             /* dTxRate  */ 0.08523187013249722,
         };
-
-        /* The Tradecraft/Freicoin test net. */
-        default_aux_pow_path = uint256S("0xe99fc44bfacee2f7e28d135845ff8a385d6d31353928d5b499700f1a2ad1b18b");
     }
 };
 
@@ -344,9 +338,6 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
         bech32_hrp = "bcrt";
-
-        /* The Tradecraft/Freicoin regtest network. */
-        default_aux_pow_path = uint256S("0xd799d41af01c1ac77e6a7793ba046a7432bb6ec250b84e2f5c6f225e05f0fc74");
     }
 
     /**
