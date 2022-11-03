288a863fd8c5a91fb63387f5295b70e9e07738d3
Mark Friedenbach <mark@friedenbach.org>
2022-11-02 22:36:15 -0700

    [CheckSig] Add SCRIPT_VERIFY_REQUIRE_VALID_SIGS flag, which enforces that every signature check passes when the signature field is non-empty (softfork safe, replaces BIP62 rule 7).
    The SCRIPT_VERIFY_REQUIRE_VALID_SIGS enforces that every signature check passes when the signature field is non-empty (softfork safe, replaces BIP62 rule 7).
    
    It is still possible for the non-VERIFY forms of CHECKSIG or CHECKMULTISIG to be executed and return false, but you MUST do so by passing in an empty (OP_0) signature.  The way this is implemented is different for the two opcodes:
    
    CHECKSIG and CHECKSIGVERIFY will abort script validation with error code SCRIPT_ERR_FAILED_SIGNATURE_CHECK if the signature validation code returns false for any reason other than the passed-in signature being a zero-length push.
    
    CHECKMULTISIG and CHECKMULTISIGVERIFY present a significant challenge to enforcing this requirement in that the original data format did not indicate which public keys were matched with which signatures, other than the ordering. For a k-of-n multisig, there are n-choose-(n-k) possibilities.  For example, a 2-of-3 multisig would have three public keys matched with two signatures, resulting in three possible assignments of pubkeys to signatures.  In the original implementation this is done by attempting to validate a signature, starting with the first public key and the first signature, and then moving to the next pubkey if validation fails.  It is not known in advance to the validator which attempts will fail.
    
    Thankfully, however, a bug in the original implementation causes an extra, unused item to be removed from stack after validation.  Since this value is given no previous consensus meaning, we use it as a bitfield to indicate which pubkeys to skip.
    
    Enforcing this requirement is a necessary precursor step to performing batch validation, since in a batch validation regime individual pubkey-signature combinations would not be checked for validity.
    
    Like bitcoin's SCRIPT_VERIFY_NULLDUMMY, this also serves as a malleability fix since the bitmask value is provided by the witness.

--- src/test/multisig_tests.cpp
+++ src/test/multisig_tests.cpp
@@ -32,12 +32,15 @@
 BOOST_FIXTURE_TEST_SUITE(multisig_tests, BasicTestingSetup)
 
 static CScript
-sign_multisig(const CScript& scriptPubKey, const std::vector<CKey>& keys, const CTransaction& transaction, int whichIn)
+sign_multisig(const CScript& scriptPubKey, const std::vector<CKey>& keys, int64_t hint, const CTransaction& transaction, int whichIn)
 {
     uint256 hash = SignatureHash(scriptPubKey, transaction, whichIn, SIGHASH_ALL, 0, 0, SigVersion::BASE);
 
     CScript result;
-    result << OP_0; // CHECKMULTISIG bug workaround
+    // Real signing code should use the MultiSigHint class to generate this
+    // value. We push a serialized integer representation only as part of the
+    // test plan for that code.
+    result << hint;
     for (const CKey &key : keys)
     {
         std::vector<unsigned char> vchSig;
@@ -51,6 +54,7 @@ sign_multisig(const CScript& scriptPubKey, const std::vector<CKey>& keys, const 
 BOOST_AUTO_TEST_CASE(multisig_verify)
 {
     unsigned int flags = SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_STRICTENC;
+    unsigned int validsigs_flags = flags | SCRIPT_VERIFY_NULLFAIL | SCRIPT_VERIFY_MULTISIG_HINT;
 
     ScriptError err;
     CKey key[4];
@@ -89,43 +93,87 @@ BOOST_AUTO_TEST_CASE(multisig_verify)
     // Test a AND b:
     keys.assign(1,key[0]);
     keys.push_back(key[1]);
-    s = sign_multisig(a_and_b, keys, CTransaction(txTo[0]), 0);
+    s = sign_multisig(a_and_b, keys, 0, CTransaction(txTo[0]), 0);
     BOOST_CHECK(VerifyScript(s, a_and_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err));
+    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+    BOOST_CHECK(VerifyScript(s, a_and_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err));
     BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
 
     for (int i = 0; i < 4; i++)
     {
         keys.assign(1,key[i]);
-        s = sign_multisig(a_and_b, keys, CTransaction(txTo[0]), 0);
+        s = sign_multisig(a_and_b, keys, 0, CTransaction(txTo[0]), 0);
         BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 1: %d", i));
+        BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_INVALID_STACK_OPERATION, ScriptErrorString(err));
+        BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 1: %d", i));
+        BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_INVALID_STACK_OPERATION, ScriptErrorString(err));
+        s = sign_multisig(a_and_b, keys, 1, CTransaction(txTo[0]), 0);
+        BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, NULL, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 3: %d", i));
+        BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_INVALID_STACK_OPERATION, ScriptErrorString(err));
+        BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 3: %d", i));
         BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_INVALID_STACK_OPERATION, ScriptErrorString(err));
 
         keys.assign(1,key[1]);
         keys.push_back(key[i]);
-        s = sign_multisig(a_and_b, keys, CTransaction(txTo[0]), 0);
+        s = sign_multisig(a_and_b, keys, 0, CTransaction(txTo[0]), 0);
         BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 2: %d", i));
         BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+        BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 2: %d", i));
+        BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_FAILED_SIGNATURE_CHECK, ScriptErrorString(err));
+        for (int j = 1; j < 5; ++j) {
+            s = sign_multisig(a_and_b, keys, j, CTransaction(txTo[0]), 0);
+            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, NULL, flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 4: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_and_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[0], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a&b 4: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
+        }
     }
 
     // Test a OR b:
     for (int i = 0; i < 4; i++)
     {
         keys.assign(1,key[i]);
-        s = sign_multisig(a_or_b, keys, CTransaction(txTo[1]), 0);
+        s = sign_multisig(a_or_b, keys, 0, CTransaction(txTo[1]), 0);
         if (i == 0 || i == 1)
         {
             BOOST_CHECK_MESSAGE(VerifyScript(s, a_or_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b: %d", i));
             BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_or_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b 1: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
         }
         else
         {
             BOOST_CHECK_MESSAGE(!VerifyScript(s, a_or_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b: %d", i));
             BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_or_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b 2: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
+        }
+        int hint = 1 + (i%2);
+        s = sign_multisig(a_or_b, keys, hint, CTransaction(txTo[1]), 0);
+        if (i == 0 || i == 1)
+        {
+            BOOST_CHECK_MESSAGE(VerifyScript(s, a_or_b, NULL, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b 3: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+            BOOST_CHECK_MESSAGE(VerifyScript(s, a_or_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b 3: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+        }
+        else
+        {
+            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_or_b, NULL, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b 4: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+            BOOST_CHECK_MESSAGE(!VerifyScript(s, a_or_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("a|b 4: %d", i));
+            BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_FAILED_SIGNATURE_CHECK, ScriptErrorString(err));
         }
     }
     s.clear();
     s << OP_0 << OP_1;
     BOOST_CHECK(!VerifyScript(s, a_or_b, nullptr, flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err));
+    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_SIG_DER, ScriptErrorString(err));
+    BOOST_CHECK(!VerifyScript(s, a_or_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err));
+    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
+    s.clear();
+    s << OP_1 << OP_1;
+    BOOST_CHECK(!VerifyScript(s, a_or_b, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[1], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err));
     BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_SIG_DER, ScriptErrorString(err));
 
 
@@ -134,16 +182,40 @@ BOOST_AUTO_TEST_CASE(multisig_verify)
         {
             keys.assign(1,key[i]);
             keys.push_back(key[j]);
-            s = sign_multisig(escrow, keys, CTransaction(txTo[2]), 0);
+            s = sign_multisig(escrow, keys, 0, CTransaction(txTo[2]), 0);
             if (i < j && i < 3 && j < 3)
             {
                 BOOST_CHECK_MESSAGE(VerifyScript(s, escrow, nullptr, flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 1: %d %d", i, j));
                 BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+                BOOST_CHECK_MESSAGE(!VerifyScript(s, escrow, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 1: %d %d", i, j));
+                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
             }
             else
             {
                 BOOST_CHECK_MESSAGE(!VerifyScript(s, escrow, nullptr, flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 2: %d %d", i, j));
                 BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+                BOOST_CHECK_MESSAGE(!VerifyScript(s, escrow, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 2: %d %d", i, j));
+                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
+            }
+            int hint = 7 & ~((1 << (2-(i%3))) | (1 << (2-(j%3))));
+            s = sign_multisig(escrow, keys, hint, CTransaction(txTo[2]), 0);
+            if (i < j && i < 3 && j < 3)
+            {
+                BOOST_CHECK_MESSAGE(VerifyScript(s, escrow, NULL, flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 3: %d %d", i, j));
+                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+                BOOST_CHECK_MESSAGE(VerifyScript(s, escrow, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 3: %d %d", i, j));
+                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_OK, ScriptErrorString(err));
+            }
+            else
+            {
+                BOOST_CHECK_MESSAGE(!VerifyScript(s, escrow, NULL, flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 4: %d %d", i, j));
+                BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_EVAL_FALSE, ScriptErrorString(err));
+                BOOST_CHECK_MESSAGE(!VerifyScript(s, escrow, NULL, validsigs_flags, MutableTransactionSignatureChecker(&txTo[2], 0, amount, txFrom.lock_height, MissingDataBehavior::ASSERT_FAIL), &err), strprintf("escrow 4: %d %d", i, j));
+                if ((i%3) == (j%3)) {
+                    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_MULTISIG_HINT, ScriptErrorString(err));
+                } else {
+                    BOOST_CHECK_MESSAGE(err == SCRIPT_ERR_FAILED_SIGNATURE_CHECK, ScriptErrorString(err));
+                }
             }
         }
 }
