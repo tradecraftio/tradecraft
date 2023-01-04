22b052b4be7fec177635dc55dbe5fddd842fa7f8
Mark Friedenbach <mark@friedenbach.org>
2023-01-04 00:16:41 -0800

    [Signet] Completely remove signet tests, as they are incompatible with future changes.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -265,7 +265,6 @@ BASE_SCRIPTS = [
     'rpc_bind.py --ipv6',
     'rpc_bind.py --nonloopback',
     'mining_basic.py',
-    'feature_signet.py',
     'wallet_bumpfee.py --legacy-wallet',
     'wallet_bumpfee.py --descriptors',
     'wallet_implicitsegwit.py --legacy-wallet',
