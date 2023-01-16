ef7e038b0f1b5cb99ce907dc80b2201d6afdff1f
Mark Friedenbach <mark@friedenbach.org>
2023-01-15 18:16:40 -0800

    [Mempool] Make full replace-by-fee policy default.
    It is not truly possible to opt-out from replace-by-fee so long as there is a single miner willing to accept the conflicting transaction.  We therefore drop support for the "opt-in" RBF semantics specified in BIP125, and instead consider ALL transactions as potentially subject to replacement.

--- test/functional/mempool_accept.py
+++ test/functional/mempool_accept.py
@@ -69,6 +69,7 @@ class MempoolAcceptanceTest(FreicoinTestFramework):
             '-permitbaremultisig=0',
             '-mempoolfullrbf=0',
             '-datacarrier=1',  # We still test OP_RETURN
+            '-mempoolfullrbf=0',
         ]] * self.num_nodes
         self.supports_cli = False
 
