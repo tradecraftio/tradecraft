f08cc9e134a62bcbb492f3f9bcddff6a692e5029
Mark Friedenbach <mark@friedenbach.org>
2023-01-17 03:17:12 -0800

    [Refheight] Require lock_heights be less than or equal to the current block height.

--- test/functional/wallet_rescan_unconfirmed.py
+++ test/functional/wallet_rescan_unconfirmed.py
@@ -68,7 +68,7 @@ class WalletRescanUnconfirmed(FreicoinTestFramework):
         # The only UTXO available to spend is tx_parent_to_reorg.
         assert_equal(len(w0_utxos), 1)
         assert_equal(w0_utxos[0]["txid"], tx_parent_to_reorg["txid"])
-        tx_child_unconfirmed_sweep = w0.sendall([ADDRESS_BCRT1_UNSPENDABLE])
+        tx_child_unconfirmed_sweep = w0.sendall([ADDRESS_BCRT1_UNSPENDABLE], options={"lockheight": max(utxo['refheight'] for utxo in w0_utxos) or 1})
         assert tx_child_unconfirmed_sweep["txid"] in node.getrawmempool()
         node.syncwithvalidationinterfacequeue()
 
