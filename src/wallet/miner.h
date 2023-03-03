37ebf4aa9dfffe5d238bf34a0b50b90a5211a684
Mark Friedenbach <mark@friedenbach.org>
2023-03-03 00:30:48 -0800

    [Stratum] Add default credentials which mine to the local wallet.
    If the username field of a stratum client authorization is empty, then the client is setup to mine from the local wallet.  Each time a work is generated, a fresh key from the wallet is used to claim the block reward.

---
+++ src/wallet/miner.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2020-2023 The Freicoin Developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef FREICOIN_WALLET_MINER_H
+#define FREICOIN_WALLET_MINER_H
+
+#include <node/context.h> // for node::NodeContext
+#include <script/standard.h> // for CTxDestination
+#include <util/translation.h> // for bilingual_str
+
+namespace wallet {
+
+//! Reserve a destination for mining.
+bool ReserveMiningDestination(const node::NodeContext& node, CTxDestination& dest, bilingual_str& error);
+//! Mark a destination as permanently used, due to a block being found.
+bool KeepMiningDestination(const CTxDestination& dest);
+//! Release any reserved destinations.
+void ReleaseMiningDestinations();
+
+} // namespace wallet
+
+#endif // FREICOIN_WALLET_MINER_H
+
+// End of File
