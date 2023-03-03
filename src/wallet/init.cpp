1f70244e893ec3f1b095dc328d8cd75b8465f626
Mark Friedenbach <mark@friedenbach.org>
2023-03-03 00:30:48 -0800

    [Stratum] Add default credentials which mine to the local wallet.
    If the username field of a stratum client authorization is empty, then the client is setup to mine from the local wallet.  Each time a work is generated, a fresh key from the wallet is used to claim the block reward.

--- src/wallet/init.cpp
+++ src/wallet/init.cpp
@@ -99,6 +99,8 @@ void WalletInit::AddWalletOptions(ArgsManager& argsman) const
 
     argsman.AddArg("-walletrejectlongchains", strprintf("Wallet will not create transactions that violate mempool chain limits (default: %u)", DEFAULT_WALLET_REJECT_LONG_CHAINS), ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::WALLET_DEBUG_TEST);
     argsman.AddArg("-walletcrosschain", strprintf("Allow reusing wallet files across chains (default: %u)", DEFAULT_WALLETCROSSCHAIN), ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::WALLET_DEBUG_TEST);
+
+    argsman.AddArg("-stratumwallet=<wallet>", "Specify wallet to use for stratum mining when the user authenticates with default credentials (default: first wallet)", ArgsManager::ALLOW_ANY, OptionsCategory::STRATUM);
 }
 
 bool WalletInit::ParameterInteraction() const
