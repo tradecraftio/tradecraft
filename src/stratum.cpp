209b4296633ad91c5678daed85c0401b998f677a
Mark Friedenbach <mark@friedenbach.org>
2023-03-12 12:04:59 -0700

    [Stratum] Add `-defaultminingaddress` for wallet-less mining.
    Default credentials are a convenient way to let the server manage mining rewards, so that individual miners don't have to all be individually configured.  This provides an alternative to `-stratumwallet`, which generates new addresses for each block but requires wallet support to be compiled in and a wallet loaded at runtime.  By using `-defaultminingaddress` the server operator can have all block rewards go to a specified address when default credentials are used.

--- src/stratum.cpp
+++ src/stratum.cpp
@@ -230,6 +230,10 @@ void UpdateSegwitCommitment(const ChainstateManager& chainman, const StratumWork
     cb_branch = BlockMerkleBranch(block2, 0);
 }
 
+//! The default address to use for mining rewards if no address is provided.
+//! Set once at startup, so it doesn't need to be protected by cs_stratum.
+static CTxDestination g_default_mining_address;
+
 //! The minimum difficulty to use for mining, for DoS protection.  This is
 //! configurable via the -miningmindifficulty option.  As of late 2023, reasonable
 //! values for this are ~1000 for ASIC mining hardware, and ~0.001 for CPU
@@ -441,9 +445,11 @@ std::string GetWorkUnit(StratumClient& client) EXCLUSIVE_LOCKS_REQUIRED(cs_strat
     }
     if (tip != tip_new || (mempool.GetTransactionsUpdated() != transactions_updated_last && (GetTime() - last_update_time) > 5) || !work_templates.count(job_id))
     {
-        CTxDestination coinbase_dest;
-        bilingual_str error;
-        wallet::ReserveMiningDestination(*g_context, coinbase_dest, error);
+        CTxDestination coinbase_dest = g_default_mining_address;
+        if (!IsValidDestination(coinbase_dest)) {
+            bilingual_str error;
+            wallet::ReserveMiningDestination(*g_context, coinbase_dest, error);
+        }
         // Update block template
         const CScript script = CScript() << OP_FALSE;
         std::unique_ptr<node::CBlockTemplate> new_work;
@@ -636,7 +642,7 @@ bool SubmitBlock(StratumClient& client, const JobId& job_id, const StratumWork& 
         // If the client is mining directly to the local wallet, consume the
         // destination used for this work unit so that new work mines to a
         // fresh address.
-        if (!IsValidDestination(client.m_addr)) {
+        if (!IsValidDestination(client.m_addr) && !IsValidDestination(g_default_mining_address)) {
             wallet::KeepMiningDestination(current_work.m_coinbase_dest);
         }
         // The client needs new work!
@@ -1131,6 +1137,21 @@ void BlockWatcher()
 bool InitStratumServer(node::NodeContext& node)
 {
     LOCK(cs_stratum);
+
+    // Either -defaultminingaddress or -stratumwallet can be set, but not both.
+    if (gArgs.IsArgSet("-defaultminingaddress") && gArgs.IsArgSet("-stratumwallet")) {
+        LogPrintf("Cannot set both -defaultminingaddress and -stratumwallet, as settings conflict.\n");
+        return false;
+    }
+    std::optional<std::string> defaultminingaddress = gArgs.GetArg("-defaultminingaddress");
+    if (defaultminingaddress) {
+        std::string error;
+        g_default_mining_address = DecodeDestination(*defaultminingaddress, error);
+        if (!IsValidDestination(g_default_mining_address)) {
+            LogPrintf("Invalid -defaultminingaddress=%s: %s\n", *defaultminingaddress, error);
+            return false;
+        }
+    }
 
     std::optional<std::string> mindiff = gArgs.GetArg("-miningmindifficulty");
     if (mindiff) {
