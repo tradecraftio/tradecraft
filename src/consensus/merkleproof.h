3541027e143c850f19b78db22fa8977ece1766e9
Mark Friedenbach <mark@friedenbach.org>
2023-10-22 17:09:33 -0700

    f '[BIP98] Add MerkleProof and MerkleTree data structures.'
    Switch to MIT license.

--- src/consensus/merkleproof.h
+++ src/consensus/merkleproof.h
@@ -1,19 +1,6 @@
 // Copyright (c) 2017-2021 The Freicoin Developers
-//
-// This program is free software: you can redistribute it and/or
-// modify it under the conjunctive terms of BOTH version 3 of the GNU
-// Affero General Public License as published by the Free Software
-// Foundation AND the MIT/X11 software license.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Affero General Public License and the MIT/X11 software license for
-// more details.
-//
-// You should have received a copy of both licenses along with this
-// program.  If not, see <https://www.gnu.org/licenses/> and
-// <http://www.opensource.org/licenses/mit-license.php>
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef FREICOIN_CONSENSUS_MERKLE_PROOF
 #define FREICOIN_CONSENSUS_MERKLE_PROOF
