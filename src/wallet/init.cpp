7e7209a247ff6d0b780ffb38bc8f1fc5e8d6103d
Mark Friedenbach <mark@friedenbach.org>
2023-10-27 14:43:25 -0700

    Revert "[FinalTx] Create block-final transaction with wallet inputs prior to soft-fork activation."
    This reverts commit a40b07051fb5701df36a3702d03c545b11dc09aa.

--- src/wallet/init.cpp
+++ src/wallet/init.cpp
@@ -96,10 +96,6 @@ void WalletInit::AddWalletOptions(ArgsManager& argsman) const
     argsman.AddArg("-walletrejectlongchains", strprintf("Wallet will not create transactions that violate mempool chain limits (default: %u)", DEFAULT_WALLET_REJECT_LONG_CHAINS), ArgsManager::ALLOW_ANY | ArgsManager::DEBUG_ONLY, OptionsCategory::WALLET_DEBUG_TEST);
 
     argsman.AddHiddenArgs({"-zapwallettxes"});
-
-    argsman.AddArg("-walletblockfinaltx=<wallet>", "Use the named wallet for constructing block-final transactions using wallet outputs prior to activation of the block-final transaction rules", ArgsManager::ALLOW_ANY, OptionsCategory::BLOCK_CREATION);
-    argsman.AddArg("-minesweepto=<address>", "Use the block-final transaction to send all value of the walletblockfinaltx wallet to the specified address in generated block templates", ArgsManager::ALLOW_ANY, OptionsCategory::BLOCK_CREATION);
-    argsman.AddArg("-carryforward=<address>", "Create an output to the specified address in the block-final transaction, typically to provide an input to the block-final transaction of the next block (default: use wallet-generated change address)", ArgsManager::ALLOW_ANY, OptionsCategory::BLOCK_CREATION);
 }
 
 bool WalletInit::ParameterInteraction() const
