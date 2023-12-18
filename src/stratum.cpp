0061aef8a973949ae9f23a00096acfb2a7552fc7
Mark Friedenbach <mark@friedenbach.org>
2023-12-18 14:12:21 -0800

    f '[Stratum] Prioritise blocks found by built-in stratum miner.'
    cs_main -> GetMutex()

--- src/stratum.cpp
+++ src/stratum.cpp
@@ -882,7 +882,7 @@ bool SubmitBlock(StratumClient& client, const JobId& job_id, const StratumWork& 
             if (res) {
                 CBlockIndex* block_index = nullptr;
                 {
-                    LOCK(cs_main);
+                    LOCK(g_context->chainman->GetMutex());
                     if (g_context->chainman->BlockIndex().count(hash)) {
                         block_index = &g_context->chainman->BlockIndex().at(hash);
                     }
