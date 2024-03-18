22903e2936ce70b11af0ae4f3cac56f5d92ca833
Mark Friedenbach <mark@friedenbach.org>
2024-03-17 20:35:16 -0700

    [FinalTx] Create block-final transaction with wallet inputs prior to soft-fork activation.

--- src/node/miner.h
+++ src/node/miner.h
@@ -218,6 +218,9 @@ int64_t UpdateTime(CBlockHeader* pblock, const Consensus::Params& consensusParam
 /** Update an old GenerateCoinbaseCommitment from CreateNewBlock after the block txs have changed */
 void RegenerateCommitments(CBlock& block, ChainstateManager& chainman);
 
+/** Modify the block-final transaction commitment */
+void UpdateBlockFinalTxCommitment(CMutableTransaction &ret, const uint256& hash);
+
 /** Apply -blockmintxfee and -blockmaxweight options from ArgsManager to BlockAssembler options. */
 void ApplyArgsManOptions(const ArgsManager& gArgs, BlockAssembler::Options& options);
 } // namespace node
