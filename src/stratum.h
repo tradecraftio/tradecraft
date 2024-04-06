af0d53469528d33464adab4785aada3e648ed395
Mark Friedenbach <mark@friedenbach.org>
2024-04-05 20:31:46 -0500

    Fix: [License] Update copyright declaration in code files.
    Switch mining code to MIT license.

--- src/stratum.h
+++ src/stratum.h
@@ -1,17 +1,6 @@
-// Copyright (c) 2020-2023 The Freicoin Developers
-// Copyright (c) 2011-2024 The Freicoin Developers
-//
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of version 3 of the GNU Affero General Public License as published
-// by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
-// details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// Copyright (c) 2020-2024 The Freicoin Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or <https://www.opensource.org/licenses/mit-license.php>.
 
 #ifndef BITCOIN_STRATUM_H
 #define BITCOIN_STRATUM_H
