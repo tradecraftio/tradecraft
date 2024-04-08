480d87e94e37571fa695c2daf7f348d2764f510b
Mark Friedenbach <mark@friedenbach.org>
2024-04-07 23:09:25 -0500

    [MergeMining/Hard-Fork] Update pow.cpp to validate merge-mined headers.
    Validating merge-mined headers is a soft-fork, but this commit also eliminates entirely the native proof-of-work challenge as part of the protocol-cleanup rule change.

--- src/test/blockfilter_index_tests.cpp
+++ src/test/blockfilter_index_tests.cpp
@@ -123,7 +123,7 @@ CBlock BuildChainTestingSetup::CreateBlock(const CBlockIndex* prev,
         entry.size = 1;
     }
 
-    while (!CheckProofOfWork(block.GetHash(), block.nBits, 0, m_node.chainman->GetConsensus())) ++block.nNonce;
+    while (!CheckProofOfWork(block, m_node.chainman->GetConsensus())) ++block.nNonce;
 
     return block;
 }
