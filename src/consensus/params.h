15ebf07aaca5f32dd7a5bf783233bda789bbc4f4
Mark Friedenbach <mark@friedenbach.org>
2024-10-21 16:14:00 -0700

    [Cleanup] Switch protocol cleanup rules activation to be height-based instead of MTP-based.

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -54,8 +54,9 @@ enum BuriedDeployment : int16_t {
     DEPLOYMENT_DERSIG,
     DEPLOYMENT_LOCKTIME,
     DEPLOYMENT_SEGWIT,
+    DEPLOYMENT_CLEANUP,
 };
-constexpr bool ValidDeployment(BuriedDeployment dep) { return dep <= DEPLOYMENT_SEGWIT; }
+constexpr bool ValidDeployment(BuriedDeployment dep) { return dep <= DEPLOYMENT_CLEANUP; }
 
 enum DeploymentPos : uint16_t {
     DEPLOYMENT_TESTDUMMY,
@@ -134,6 +135,8 @@ struct Params {
      * Note that segwit v0 script rules are enforced on all blocks except the
      * BIP 16 exception blocks. */
     int SegwitHeight;
+    /** Block height at which the protocl cleanup rule changes become active */
+    int CleanupHeight;
     /** Don't warn about unknown BIP 9 activations below this height.
      * This prevents us from warning about the locktime and segwit activations. */
     int MinBIP9WarningHeight;
@@ -198,6 +201,8 @@ struct Params {
             return LockTimeHeight;
         case DEPLOYMENT_SEGWIT:
             return SegwitHeight;
+        case DEPLOYMENT_CLEANUP:
+            return CleanupHeight;
         } // no default case, so the compiler can warn about missing cases
         return std::numeric_limits<int>::max();
     }
