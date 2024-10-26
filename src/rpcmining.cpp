13925549e85ab87360d017676eb9e89e0051ce86
Mark Friedenbach <mark@friedenbach.org>
2024-10-26 09:23:46 -0700

    f '[Hard-Fork] Implement time-activated "protocol cleanup" flag-day hard-fork, set to activate in April 2021.'
    Don't mine old pow for cleanup blocks after activation.

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -162,10 +162,12 @@ UniValue generate(const UniValue& params, bool fHelp)
             LOCK(cs_main);
             IncrementExtraNonce(pblock, chainActive.Tip(), nExtraNonce);
         }
-        while (!CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus())) {
-            // Yes, there is a chance every nonce could fail to satisfy the -regtest
-            // target -- 1 in 2^(2^32). That ain't gonna happen.
-            ++pblock->nNonce;
+        if (!IsProtocolCleanupActive(Params().GetConsensus(), *pblock)) {
+            while (!CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus())) {
+                // Yes, there is a chance every nonce could fail to satisfy the -regtest
+                // target -- 1 in 2^(2^32). That ain't gonna happen.
+                ++pblock->nNonce;
+            }
         }
         CValidationState state;
         if (!ProcessNewBlock(state, Params(), NULL, pblock, true, NULL))
