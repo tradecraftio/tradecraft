c91390253f7337ad7d8e7f845e8bf2209926d205
Mark Friedenbach <mark@friedenbach.org>
2024-10-26 09:23:46 -0700

    f '[Hard-Fork] Implement time-activated "protocol cleanup" flag-day hard-fork, set to activate in April 2021.'
    Don't mine cleanup blocks after activation.

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -141,9 +141,11 @@ static bool GenerateBlock(ChainstateManager& chainman, CBlock& block, uint64_t& 
         IncrementExtraNonce(&block, chainparams.GetConsensus(), chainman.ActiveChain().Tip(), extra_nonce, aux_hash2);
     }
 
-    while (max_tries > 0 && block.nNonce < std::numeric_limits<uint32_t>::max() && !CheckProofOfWork(block, chainparams.GetConsensus()) && !ShutdownRequested()) {
-        ++block.nNonce;
-        --max_tries;
+    if (!IsProtocolCleanupActive(chainparams.GetConsensus(), block)) {
+        while (max_tries > 0 && block.nNonce < std::numeric_limits<uint32_t>::max() && !CheckProofOfWork(block, chainparams.GetConsensus()) && !ShutdownRequested()) {
+            ++block.nNonce;
+            --max_tries;
+        }
     }
     if (max_tries == 0 || ShutdownRequested()) {
         return false;
