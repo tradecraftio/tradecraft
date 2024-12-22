0d56c71da8260f3b959e9c8950c60eb772d67127
Mark Friedenbach <mark@friedenbach.org>
2024-12-21 23:43:40 -0800

    [Segwit] Allow arbitrary commitment tree structure.
    The witness root is allowed to be placed at an arbitrary position up to seven layers deep in a Merkle tree structure.  The witness nonce is now the branch through the commitment tree to the witness root, and a single byte is added to the commitment output specifying this path in compact form.  This allows other consensus commitments to be added in the future with a minimal number of bytes and without requiring a certain position for each commitment in the tree.

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -655,9 +655,9 @@ public:
         m_assumeutxo_data = {
             {   // For use by unit tests
                 .height = 110,
-                .hash_serialized = AssumeutxoHash{uint256{"aa1e857667a2d819b99fddef3c9dbd328762870e25b88ad9580be5533e9e4375"}},
+                .hash_serialized = AssumeutxoHash{uint256{"4ab2aab6f9ccc48de7d1175ec57bbbaa8b0249ba036962d8bed7e19f4b270429"}},
                 .m_chain_tx_count = 121,
-                .blockhash = consteval_ctor(uint256{"67151c446b78190bbf6e22419a4155df6e00ab35e250ab87a358f354aa6a2d5d"}),
+                .blockhash = consteval_ctor(uint256{"4180e1c5c6cb4acb76afe13b70d47bbe8ce63b68334c245fb5a215454055f753"}),
             },
             {
                 // For use by fuzz target src/test/fuzz/utxo_snapshot.cpp
@@ -669,9 +669,9 @@ public:
             {
                 // For use by test/functional/feature_assumeutxo.py
                 .height = 299,
-                .hash_serialized = AssumeutxoHash{uint256{"4a6005c672b777d65eb6dcb9bac646f7bd2110ba71fa481f2ea1f5bb9cda9baf"}},
+                .hash_serialized = AssumeutxoHash{uint256{"2418e9dbd577105424f9ac80cdcfe33d6c0c1c4b74bfaf27d9b50fd93ad9200e"}},
                 .m_chain_tx_count = 533,
-                .blockhash = consteval_ctor(uint256{"667794d0a368d67f27e001c1ae145c774a1bf72e94529f3567468c5f05052fce"}),
+                .blockhash = consteval_ctor(uint256{"540207b8d6157b8503d6686e4781de7c892407e323fdd118e0017d6bc1a594f7"}),
             },
         };
 
